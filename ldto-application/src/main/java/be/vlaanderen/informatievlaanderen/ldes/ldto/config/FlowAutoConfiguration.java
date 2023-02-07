45a3ad4bc7b6fa7f274ef65d0d750d87c498aeb9
Jan Robert <jan.robert@cegeka.com>
2023-02-07 13:21:31 +0100

    feat: VSDSPUB-461: Create only required beans + Readme

--- ldto-application/src/main/java/be/vlaanderen/informatievlaanderen/ldes/ldto/config/FlowAutoConfiguration.java
+++ ldto-application/src/main/java/be/vlaanderen/informatievlaanderen/ldes/ldto/config/FlowAutoConfiguration.java
@@ -1,18 +1,14 @@
 package be.vlaanderen.informatievlaanderen.ldes.ldto.config;
 
 import be.vlaanderen.informatievlaanderen.ldes.ldto.converter.ModelHttpConverter;
-import be.vlaanderen.informatievlaanderen.ldes.ldto.input.LdtoHttpIn;
-import be.vlaanderen.informatievlaanderen.ldes.ldto.output.LdtoConsoleOut;
-import be.vlaanderen.informatievlaanderen.ldes.ldto.output.LdtoHttpOut;
 import be.vlaanderen.informatievlaanderen.ldes.ldto.services.ComponentExecutor;
 import be.vlaanderen.informatievlaanderen.ldes.ldto.services.ComponentExecutorImpl;
-import be.vlaanderen.informatievlaanderen.ldes.ldto.transformer.SparqlConstructTransformer;
 import be.vlaanderen.informatievlaanderen.ldes.ldto.types.LdtoInput;
 import be.vlaanderen.informatievlaanderen.ldes.ldto.types.LdtoOutput;
 import be.vlaanderen.informatievlaanderen.ldes.ldto.types.LdtoTransformer;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.support.DefaultListableBeanFactory;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
-import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
@@ -26,37 +22,39 @@ import java.util.stream.Collectors;
 public class FlowAutoConfiguration {
 
 	@Autowired
-	private ApplicationContext applicationContext;
+	private DefaultListableBeanFactory beanFactory;
 
 	@Bean
 	@ConditionalOnMissingBean(LdtoInput.class)
-	public LdtoInput ldtoInput(OrchestratorConfig orchestratorConfig, ComponentExecutor componentExecutor) {
-		return switch (orchestratorConfig.getInput().getName()) {
-			case "LdtoHttpIn" -> new LdtoHttpIn(componentExecutor);
-			default -> throw new RuntimeException();
-		};
-	}
-
-	public LdtoOutput ldtoOutput(OrchestratorConfig orchestratorConfig) {
-		return switch (orchestratorConfig.getOutput().getName()) {
-			case "LdtoConsoleOut" -> new LdtoConsoleOut(orchestratorConfig);
-			case "LdtoHttpOut" -> new LdtoHttpOut(orchestratorConfig);
-			default -> throw new RuntimeException();
-		};
-	}
-
-	public LdtoTransformer ldtoTransformer(String transformer, Map<String, String> config) {
-		return switch (transformer) {
-			case "SparqlConstructTransformer" -> new SparqlConstructTransformer(config);
-			default -> throw new RuntimeException();
-		};
+	public LdtoInput ldtoInput(OrchestratorConfig orchestratorConfig)
+			throws ClassNotFoundException {
+		return (LdtoInput) beanFactory.createBean(Class.forName(orchestratorConfig.getInput().getName()));
 	}
 
 	@Bean
-	public ComponentExecutor componentExecutor(final OrchestratorConfig orchestratorConfig) {
-		LdtoOutput ldtoOutput = ldtoOutput(orchestratorConfig);
+	@ConditionalOnMissingBean(LdtoOutput.class)
+	public LdtoOutput ldtoOutput(OrchestratorConfig orchestratorConfig, DefaultListableBeanFactory beanFactory)
+			throws ClassNotFoundException {
+		return (LdtoOutput) beanFactory.createBean(Class.forName(orchestratorConfig.getOutput().getName()));
+	}
+
+	public LdtoTransformer ldtoTransformer(String transformer, Map<String, String> config)
+			throws ClassNotFoundException {
+		LdtoTransformer ldtoTransformer = (LdtoTransformer) beanFactory.createBean(Class.forName(transformer));
+		ldtoTransformer.init(config);
+		return ldtoTransformer;
+	}
+
+	@Bean
+	public ComponentExecutor componentExecutor(final OrchestratorConfig orchestratorConfig, LdtoOutput ldtoOutput) {
 		List<LdtoTransformer> ldtoTransformers = orchestratorConfig.getTransformers().stream()
-				.map(componentDefinition -> ldtoTransformer(componentDefinition.getName(), componentDefinition.getConfig()))
+				.map(componentDefinition -> {
+					try {
+						return ldtoTransformer(componentDefinition.getName(), componentDefinition.getConfig());
+					} catch (ClassNotFoundException e) {
+						throw new RuntimeException(e);
+					}
+				})
 				.collect(Collectors.toList());
 		return new ComponentExecutorImpl(ldtoTransformers, ldtoOutput);
 	}
--- ldto-starter-kit/src/main/java/be/vlaanderen/informatievlaanderen/ldes/ldto/transformer/SparqlConstructTransformer.java
+++ ldto-starter-kit/src/main/java/be/vlaanderen/informatievlaanderen/ldes/ldto/transformer/SparqlConstructTransformer.java
@@ -13,15 +13,15 @@ import java.util.Objects;
 import static be.vlaanderen.informatievlaanderen.ldes.ldto.LdtoConstants.*;
 
 public class SparqlConstructTransformer implements LdtoTransformer {
-	private final Query query;
-	private final boolean inferMode;
+	private Query query;
+	private boolean inferMode;
 
-	public SparqlConstructTransformer(Map<String, String> config) {
+	@Override
+	public void init(Map<String, String> config) {
 		this.query = QueryFactory.create(Objects.requireNonNull(config.get(QUERY), QUERY_VALIDATION_MSG));
 		this.inferMode = Boolean.parseBoolean(config.getOrDefault(INFER, String.valueOf(false)));
 	}
 
-	@Override
 	public Model execute(Model linkedDataModel) {
 		try (QueryExecution qexec = QueryExecutionFactory.create(query, linkedDataModel)) {
 			Model resultModel = qexec.execConstruct();
