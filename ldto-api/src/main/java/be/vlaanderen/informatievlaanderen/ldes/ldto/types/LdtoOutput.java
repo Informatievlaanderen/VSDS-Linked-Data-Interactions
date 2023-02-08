31132cb844ec029138ef724b4a7e6f226b0e3e4b
Jan Robert <15772440+Yalz@users.noreply.github.com>
2023-02-08 14:52:37 +0100

    feat: support multiple + parallel outputs (#3)

--- ldto-api/src/main/java/be/vlaanderen/informatievlaanderen/ldes/ldto/types/LdtoOutput.java
+++ ldto-api/src/main/java/be/vlaanderen/informatievlaanderen/ldes/ldto/types/LdtoOutput.java
@@ -2,6 +2,9 @@ package be.vlaanderen.informatievlaanderen.ldes.ldto.types;
 
 import org.apache.jena.rdf.model.Model;
 
+import java.util.Map;
+
 public interface LdtoOutput {
+	void init(Map<String, String> config);
 	void sendLinkedData(Model linkedDataModel);
 }
--- ldto-application/src/main/java/be/vlaanderen/informatievlaanderen/ldes/ldto/config/FlowAutoConfiguration.java
+++ ldto-application/src/main/java/be/vlaanderen/informatievlaanderen/ldes/ldto/config/FlowAutoConfiguration.java
@@ -3,6 +3,7 @@ package be.vlaanderen.informatievlaanderen.ldes.ldto.config;
 import be.vlaanderen.informatievlaanderen.ldes.ldto.converter.ModelHttpConverter;
 import be.vlaanderen.informatievlaanderen.ldes.ldto.services.ComponentExecutor;
 import be.vlaanderen.informatievlaanderen.ldes.ldto.services.ComponentExecutorImpl;
+import be.vlaanderen.informatievlaanderen.ldes.ldto.types.ComponentDefinition;
 import be.vlaanderen.informatievlaanderen.ldes.ldto.types.LdtoInput;
 import be.vlaanderen.informatievlaanderen.ldes.ldto.types.LdtoOutput;
 import be.vlaanderen.informatievlaanderen.ldes.ldto.types.LdtoTransformer;
@@ -32,36 +33,43 @@ public class FlowAutoConfiguration {
 	}
 
 	@Bean
-	@ConditionalOnMissingBean(LdtoOutput.class)
-	public LdtoOutput ldtoOutput(OrchestratorConfig orchestratorConfig, DefaultListableBeanFactory beanFactory)
-			throws ClassNotFoundException {
-		return (LdtoOutput) beanFactory.createBean(Class.forName(orchestratorConfig.getOutput().getName()));
-	}
-
-	public LdtoTransformer ldtoTransformer(String transformer, Map<String, String> config)
-			throws ClassNotFoundException {
-		LdtoTransformer ldtoTransformer = (LdtoTransformer) beanFactory.createBean(Class.forName(transformer));
-		ldtoTransformer.init(config);
-		return ldtoTransformer;
-	}
-
-	@Bean
-	public ComponentExecutor componentExecutor(final OrchestratorConfig orchestratorConfig, LdtoOutput ldtoOutput) {
-		List<LdtoTransformer> ldtoTransformers = orchestratorConfig.getTransformers().stream()
-				.map(componentDefinition -> {
-					try {
-						return ldtoTransformer(componentDefinition.getName(), componentDefinition.getConfig());
-					} catch (ClassNotFoundException e) {
-						throw new RuntimeException(e);
-					}
-				})
-				.collect(Collectors.toList());
-		return new ComponentExecutorImpl(ldtoTransformers, ldtoOutput);
+	public ComponentExecutor componentExecutor(final OrchestratorConfig orchestratorConfig) {
+		List<LdtoTransformer> ldtoTransformers = orchestratorConfig.getTransformers()
+				.stream()
+				.map(this::ldtoTransformer)
+				.toList();
+		List<LdtoOutput> ldtoOutputs = orchestratorConfig.getOutputs()
+				.stream()
+				.map(this::ldtoOutput)
+				.toList();
+		return new ComponentExecutorImpl(ldtoTransformers, ldtoOutputs);
 	}
 
 	@Bean
 	public ModelHttpConverter memberConverter() {
 		return new ModelHttpConverter();
+	}
+
+	private LdtoTransformer ldtoTransformer(ComponentDefinition componentDefinition) {
+		try {
+			LdtoTransformer ldtoTransformer = (LdtoTransformer) beanFactory.createBean(Class.forName(
+					componentDefinition.getName()));
+			ldtoTransformer.init(componentDefinition.getConfig());
+			return ldtoTransformer;
+		} catch (ClassNotFoundException e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+	private LdtoOutput ldtoOutput(ComponentDefinition componentDefinition) {
+		try {
+			LdtoOutput ldtoOutput = (LdtoOutput) beanFactory.createBean(Class.forName(
+					componentDefinition.getName()));
+			ldtoOutput.init(componentDefinition.getConfig());
+			return ldtoOutput;
+		} catch (ClassNotFoundException e) {
+			throw new RuntimeException(e);
+		}
 	}
 
 }
