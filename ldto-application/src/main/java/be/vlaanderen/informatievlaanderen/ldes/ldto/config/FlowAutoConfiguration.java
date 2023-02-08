2da84e418271d3f2de6bcca2123d7f481af67559
Jan Robert <jan.robert@cegeka.com>
2023-02-08 16:07:04 +0100

    fix: cleanup spring deps from API

--- ldto-application/src/main/java/be/vlaanderen/informatievlaanderen/ldes/ldto/config/FlowAutoConfiguration.java
+++ ldto-application/src/main/java/be/vlaanderen/informatievlaanderen/ldes/ldto/config/FlowAutoConfiguration.java
@@ -1,6 +1,5 @@
 package be.vlaanderen.informatievlaanderen.ldes.ldto.config;
 
-import be.vlaanderen.informatievlaanderen.ldes.ldto.services.ModelHttpConverter;
 import be.vlaanderen.informatievlaanderen.ldes.ldto.services.ComponentExecutor;
 import be.vlaanderen.informatievlaanderen.ldes.ldto.services.ComponentExecutorImpl;
 import be.vlaanderen.informatievlaanderen.ldes.ldto.types.ComponentDefinition;
@@ -41,11 +40,6 @@ public class FlowAutoConfiguration {
 				.map(this::ldtoOutput)
 				.toList();
 		return new ComponentExecutorImpl(ldtoTransformers, ldtoOutputs);
-	}
-
-	@Bean
-	public ModelHttpConverter memberConverter() {
-		return new ModelHttpConverter();
 	}
 
 	private LdtoTransformer ldtoTransformer(ComponentDefinition componentDefinition) {
