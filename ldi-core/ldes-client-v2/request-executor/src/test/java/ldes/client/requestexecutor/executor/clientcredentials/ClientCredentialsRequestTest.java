f4f0ec776e41bca49b40b48c0889ea8a6220f007
WLefever <wouter.lefever@cegeka.com>
2023-03-09 16:59:27 +0100

    temp comment test

--- ldi-core/ldes-client-v2/request-executor/src/test/java/ldes/client/requestexecutor/executor/clientcredentials/ClientCredentialsRequestTest.java
+++ ldi-core/ldes-client-v2/request-executor/src/test/java/ldes/client/requestexecutor/executor/clientcredentials/ClientCredentialsRequestTest.java
@@ -1,33 +1,31 @@
-// package ldes.client.requestexecutor.executor.clientcredentials;
-//
-// import ldes.client.requestexecutor.domain.valueobjects.Request;
-// import ldes.client.requestexecutor.domain.valueobjects.RequestHeader;
-// import ldes.client.requestexecutor.domain.valueobjects.RequestHeaders;
-// import org.junit.jupiter.api.Test;
-//
-// import java.util.List;
-//
-// import com.github.scribejava.core.model.OAuthRequest;
-//
-// import static org.junit.jupiter.api.Assertions.assertEquals;
-//
-// class ClientCredentialsRequestTest {
-//
-// @Test
-// void whenGetOAuthRequest_shouldContainUrlAndHeaders() {
-// RequestHeaders requestHeaders = new RequestHeaders(
-// List.of(
-// new RequestHeader("key", "value"),
-// new RequestHeader("otherKey", "otherValue")));
-//
-// Request request = new Request("url", requestHeaders);
-// ClientCredentialsRequest credentialsRequest = new
-// ClientCredentialsRequest(request);
-// OAuthRequest oAuthRequest = credentialsRequest.getOAuthRequest();
-//
-// assertEquals("url", oAuthRequest.getUrl());
-// requestHeaders
-// .forEach(header -> assertEquals(header.getValue(),
-// oAuthRequest.getHeaders().get(header.getKey())));
-// }
-// }
+package ldes.client.requestexecutor.executor.clientcredentials;
+
+import ldes.client.requestexecutor.domain.valueobjects.Request;
+import ldes.client.requestexecutor.domain.valueobjects.RequestHeader;
+import ldes.client.requestexecutor.domain.valueobjects.RequestHeaders;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import com.github.scribejava.core.model.OAuthRequest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class ClientCredentialsRequestTest {
+
+	@Test
+	void whenGetOAuthRequest_shouldContainUrlAndHeaders() {
+		RequestHeaders requestHeaders = new RequestHeaders(
+				List.of(
+						new RequestHeader("key", "value"),
+						new RequestHeader("otherKey", "otherValue")));
+
+		Request request = new Request("url", requestHeaders);
+		ClientCredentialsRequest credentialsRequest = new ClientCredentialsRequest(request);
+		OAuthRequest oAuthRequest = credentialsRequest.getOAuthRequest();
+
+		assertEquals("url", oAuthRequest.getUrl());
+		requestHeaders
+				.forEach(header -> assertEquals(header.getValue(), oAuthRequest.getHeaders().get(header.getKey())));
+	}
+}
