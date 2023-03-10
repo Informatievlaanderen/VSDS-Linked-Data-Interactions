da24b0d00952885d851bf8fe6f7253454184ae8a
WLefever <wouter.lefever@cegeka.com>
2023-03-10 15:01:41 +0100

    move config

--- ldi-orchestrator/ldio-connectors/ldio-ldes-client/src/main/java/be/vlaanderen/informatievlaanderen/ldes/ldi/client/LdesClientRunner.java
+++ ldi-orchestrator/ldio-connectors/ldio-ldes-client/src/main/java/be/vlaanderen/informatievlaanderen/ldes/ldi/client/LdesClientRunner.java
@@ -1,24 +1,68 @@
 package be.vlaanderen.informatievlaanderen.ldes.ldi.client;
 
+import be.vlaanderen.informatievlaanderen.ldes.ldi.config.ComponentProperties;
 import be.vlaanderen.informatievlaanderen.ldes.ldi.services.ComponentExecutor;
+import ldes.client.requestexecutor.executor.RequestExecutor;
+import ldes.client.treenodefetcher.TreeNodeFetcher;
+import ldes.client.treenodesupplier.LdesProvider;
 import ldes.client.treenodesupplier.MemberSupplier;
+import ldes.client.treenodesupplier.TreeNodeProcessor;
+import ldes.client.treenodesupplier.domain.valueobject.Ldes;
+import ldes.client.treenodesupplier.repository.inmemory.InMemoryMemberRepository;
+import ldes.client.treenodesupplier.repository.inmemory.InMemoryTreeNodeRecordRepository;
+import ldes.client.treenodesupplier.repository.sqlite.SqliteMemberRepository;
+import ldes.client.treenodesupplier.repository.sqlite.SqliteTreeNodeRepository;
+import org.apache.jena.riot.Lang;
+import org.apache.jena.riot.RDFLanguages;
+
+import static be.vlaanderen.informatievlaanderen.ldes.ldi.client.LdioLdesClientProperties.*;
 
 public class LdesClientRunner implements Runnable {
-	private final MemberSupplier memberSupplier;
 	private final ComponentExecutor componentExecutor;
+	private final RequestExecutor requestExecutor;
+	private final ComponentProperties properties;
 
 	private boolean threadrunning = true;
 
-	public LdesClientRunner(MemberSupplier memberSupplier, ComponentExecutor componentExecutor) {
-		this.memberSupplier = memberSupplier;
+	public LdesClientRunner(RequestExecutor requestExecutor, ComponentProperties properties,
+			ComponentExecutor componentExecutor) {
 		this.componentExecutor = componentExecutor;
+		this.requestExecutor = requestExecutor;
+		this.properties = properties;
 	}
 
 	@Override
 	public void run() {
+		String targetUrl = properties.getProperty(URL);
+		Lang sourceFormat = properties.getOptionalProperty(SOURCE_FORMAT)
+				.map(RDFLanguages::nameToLang)
+				.orElse(Lang.JSONLD);
+		String state = properties.getOptionalProperty(STATE)
+				.orElse("memory");
+		boolean keepState = properties.getOptionalProperty(KEEP_STATE)
+				.map(Boolean::valueOf)
+				.orElse(false);
+		Ldes ldes = new LdesProvider(requestExecutor).getLdes(targetUrl, sourceFormat);
+		TreeNodeProcessor treeNodeProcessor = getTreeNodeProcessor(state, keepState, requestExecutor, ldes);
+		MemberSupplier memberSupplier = new MemberSupplier(treeNodeProcessor);
 		while (threadrunning) {
 			componentExecutor.transformLinkedData(memberSupplier.get().getModel());
 		}
+	}
+
+	private TreeNodeProcessor getTreeNodeProcessor(String state, boolean keepState, RequestExecutor requestExecutor,
+			Ldes ldes) {
+		TreeNodeProcessor treeNodeProcessor;
+		if (state.equals("sqlite")) {
+			treeNodeProcessor = new TreeNodeProcessor(ldes, new SqliteTreeNodeRepository(),
+					new SqliteMemberRepository(),
+					new TreeNodeFetcher(requestExecutor), keepState);
+		} else {
+			treeNodeProcessor = new TreeNodeProcessor(ldes, new InMemoryTreeNodeRecordRepository(),
+					new InMemoryMemberRepository(),
+					new TreeNodeFetcher(requestExecutor), keepState);
+		}
+		return treeNodeProcessor;
 	}
 
 	public void stopThread() {
--- ldi-orchestrator/ldio-connectors/ldio-ldes-client/src/main/java/be/vlaanderen/informatievlaanderen/ldes/ldi/client/LdioLdesClientAutoConfig.java
+++ ldi-orchestrator/ldio-connectors/ldio-ldes-client/src/main/java/be/vlaanderen/informatievlaanderen/ldes/ldi/client/LdioLdesClientAutoConfig.java
@@ -7,19 +7,6 @@ import be.vlaanderen.informatievlaanderen.ldes.ldi.types.LdiComponent;
 import ldes.client.requestexecutor.domain.valueobjects.AuthStrategy;
 import ldes.client.requestexecutor.domain.valueobjects.executorsupplier.RequestExecutorFactory;
 import ldes.client.requestexecutor.executor.RequestExecutor;
-import ldes.client.treenodefetcher.TreeNodeFetcher;
-import ldes.client.treenodesupplier.LdesProvider;
-import ldes.client.treenodesupplier.MemberSupplier;
-import ldes.client.treenodesupplier.TreeNodeProcessor;
-import ldes.client.treenodesupplier.domain.valueobject.Ldes;
-import ldes.client.treenodesupplier.repository.inmemory.InMemoryMemberRepository;
-import ldes.client.treenodesupplier.repository.inmemory.InMemoryTreeNodeRecordRepository;
-import ldes.client.treenodesupplier.repository.sqlite.SqliteMemberRepository;
-import ldes.client.treenodesupplier.repository.sqlite.SqliteTreeNodeRepository;
-import org.apache.jena.riot.Lang;
-import org.apache.jena.riot.RDFLanguages;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
@@ -27,7 +14,9 @@ import org.springframework.context.annotation.Configuration;
 
 import java.util.Optional;
 
-import static ldes.client.requestexecutor.domain.valueobjects.AuthStrategy.NO_AUTH;
+import static be.vlaanderen.informatievlaanderen.ldes.ldi.client.LdioLdesClientProperties.*;
+import static be.vlaanderen.informatievlaanderen.ldes.ldi.client.LdioLdesClientProperties.API_KEY;
+import static ldes.client.requestexecutor.domain.valueobjects.AuthStrategy.*;
 
 @Configuration
 @EnableConfigurationProperties()
@@ -39,19 +28,6 @@ public class LdioLdesClientAutoConfig {
 	}
 
 	public static class LdioLdesClientConfigurator implements LdioConfigurator {
-
-		public static final String CLIENT_ID = "clientId";
-		public static final String CLIENT_SECRET = "clientSecret";
-		public static final String TOKEN_ENDPOINT = "tokenEndpoint";
-		public static final String OAUTH_SCOPE = "oAuthScope";
-		public static final String AUTHENTICATION = "authentication";
-		Logger logger = LoggerFactory.getLogger(LdioLdesClientConfigurator.class);
-		public static final String URL = "url";
-		public static final String SOURCE_FORMAT = "sourceFormat";
-		public static final String STATE = "state";
-		public static final String KEEP_STATE = "keepState";
-		public static final String API_KEY = "apiKey";
-		public static final String API_KEY_HEADER = "apiKeyHeader";
 		private final ComponentExecutor componentExecutor;
 
 		public LdioLdesClientConfigurator(ComponentExecutor componentExecutor) {
@@ -60,36 +36,9 @@ public class LdioLdesClientAutoConfig {
 
 		@Override
 		public LdiComponent configure(ComponentProperties properties) {
-			String targetUrl = properties.getProperty(URL);
-			Lang sourceFormat = properties.getOptionalProperty(SOURCE_FORMAT)
-					.map(RDFLanguages::nameToLang)
-					.orElse(Lang.JSONLD);
-			String state = properties.getOptionalProperty(STATE)
-					.orElse("memory");
-			boolean keepState = properties.getOptionalProperty(KEEP_STATE)
-					.map(Boolean::valueOf)
-					.orElse(false);
 			RequestExecutor requestExecutor = getRequestExecutor(properties);
-			Ldes ldes = new LdesProvider(requestExecutor).getLdes(targetUrl, sourceFormat);
-			TreeNodeProcessor treeNodeProcessor = getTreeNodeProcessor(state, keepState, requestExecutor, ldes);
-			MemberSupplier memberSupplier = new MemberSupplier(treeNodeProcessor);
-			LdesClientRunner ldesClientRunner = new LdesClientRunner(memberSupplier, componentExecutor);
+			LdesClientRunner ldesClientRunner = new LdesClientRunner(requestExecutor, properties, componentExecutor);
 			return new LdioLdesClient(componentExecutor, ldesClientRunner);
-		}
-
-		private TreeNodeProcessor getTreeNodeProcessor(String state, boolean keepState, RequestExecutor requestExecutor,
-				Ldes ldes) {
-			TreeNodeProcessor treeNodeProcessor;
-			if (state.equals("sqlite")) {
-				treeNodeProcessor = new TreeNodeProcessor(ldes, new SqliteTreeNodeRepository(),
-						new SqliteMemberRepository(),
-						new TreeNodeFetcher(requestExecutor), keepState);
-			} else {
-				treeNodeProcessor = new TreeNodeProcessor(ldes, new InMemoryTreeNodeRecordRepository(),
-						new InMemoryMemberRepository(),
-						new TreeNodeFetcher(requestExecutor), keepState);
-			}
-			return treeNodeProcessor;
 		}
 
 		private RequestExecutor getRequestExecutor(ComponentProperties componentProperties) {
