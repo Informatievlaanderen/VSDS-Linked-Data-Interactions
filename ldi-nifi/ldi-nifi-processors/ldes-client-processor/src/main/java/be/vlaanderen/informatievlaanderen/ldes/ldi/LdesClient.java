c5c640c305aeda0493215ce4d6cca04032fbd0d2
WLefever <wouter.lefever@cegeka.com>
2023-03-10 09:15:41 +0100

    update nifi

--- ldi-nifi/ldi-nifi-processors/ldes-client-processor/src/main/java/be/vlaanderen/informatievlaanderen/ldes/ldi/LdesClient.java
+++ ldi-nifi/ldi-nifi-processors/ldes-client-processor/src/main/java/be/vlaanderen/informatievlaanderen/ldes/ldi/LdesClient.java
@@ -1,22 +1,28 @@
 package be.vlaanderen.informatievlaanderen.ldes.ldi;
 
-import be.vlaanderen.informatievlaanderen.ldes.client.LdesClientImplFactory;
-import be.vlaanderen.informatievlaanderen.ldes.client.config.LdesClientConfig;
-import be.vlaanderen.informatievlaanderen.ldes.client.converters.ModelConverter;
-import be.vlaanderen.informatievlaanderen.ldes.client.endpointrequester.EndpointRequester;
-import be.vlaanderen.informatievlaanderen.ldes.client.endpointrequester.endpoint.ApiKey;
-import be.vlaanderen.informatievlaanderen.ldes.client.endpointrequester.endpoint.Endpoint;
-import be.vlaanderen.informatievlaanderen.ldes.client.endpointrequester.startingnode.StartingNode;
-import be.vlaanderen.informatievlaanderen.ldes.client.exceptions.LdesPropertyNotFoundException;
-import be.vlaanderen.informatievlaanderen.ldes.client.services.LdesService;
-import be.vlaanderen.informatievlaanderen.ldes.client.valueobjects.LdesFragment;
 import be.vlaanderen.informatievlaanderen.ldes.ldi.config.LdesProcessorProperties;
+import be.vlaanderen.informatievlaanderen.ldes.ldi.domain.valueobjects.LdesProperties;
 import be.vlaanderen.informatievlaanderen.ldes.ldi.processors.services.FlowManager;
+import be.vlaanderen.informatievlaanderen.ldes.ldi.services.LdesPropertiesExtractor;
+import ldes.client.requestexecutor.domain.valueobjects.ApiKeyConfig;
+import ldes.client.requestexecutor.domain.valueobjects.DefaultConfig;
+import ldes.client.requestexecutor.executor.RequestExecutor;
+import ldes.client.startingtreenode.StartingTreeNodeFinder;
+import ldes.client.startingtreenode.domain.valueobjects.Endpoint;
+import ldes.client.startingtreenode.domain.valueobjects.TreeNode;
+import ldes.client.treenodefetcher.TreeNodeFetcher;
+import ldes.client.treenodesupplier.MemberSupplier;
+import ldes.client.treenodesupplier.TreeNodeProcessor;
+import ldes.client.treenodesupplier.domain.entities.SuppliedMember;
+import ldes.client.treenodesupplier.domain.valueobject.Ldes;
+import ldes.client.treenodesupplier.repository.sqlite.SqliteMemberRepository;
+import ldes.client.treenodesupplier.repository.sqlite.SqliteTreeNodeRepository;
+import org.apache.jena.rdf.model.Model;
 import org.apache.jena.riot.Lang;
+import org.apache.jena.riot.RDFDataMgr;
 import org.apache.nifi.annotation.behavior.Stateful;
 import org.apache.nifi.annotation.documentation.CapabilityDescription;
 import org.apache.nifi.annotation.documentation.Tags;
-import org.apache.nifi.annotation.lifecycle.OnAdded;
 import org.apache.nifi.annotation.lifecycle.OnRemoved;
 import org.apache.nifi.annotation.lifecycle.OnScheduled;
 import org.apache.nifi.components.PropertyDescriptor;
@@ -30,7 +36,9 @@ import org.apache.nifi.processor.exception.ProcessException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.StringWriter;
 import java.util.List;
+import java.util.Optional;
 import java.util.Set;
 
 import static be.vlaanderen.informatievlaanderen.ldes.ldi.config.LdesProcessorProperties.*;
@@ -43,11 +51,9 @@ import static be.vlaanderen.informatievlaanderen.ldes.ldi.config.LdesProcessorRe
 public class LdesClient extends AbstractProcessor {
 
 	private static final Logger LOGGER = LoggerFactory.getLogger(LdesClient.class);
-
-	protected String identifier = null;
-	protected LdesClientConfig config = new LdesClientConfig();
-	protected LdesService ldesService;
-	private final EndpointRequester endpointRequester = new EndpointRequester();
+	TreeNodeProcessor treeNodeProcessor;
+	MemberSupplier memberSupplier;
+	private LdesProperties ldesProperties;
 
 	@Override
 	public Set<Relationship> getRelationships() {
@@ -60,84 +66,74 @@ public class LdesClient extends AbstractProcessor {
 				STREAM_TIMESTAMP_PATH_PROPERTY, STREAM_VERSION_OF_PROPERTY, STREAM_SHAPE_PROPERTY);
 	}
 
-	@OnAdded
-	public void onAdded() {
-		if (identifier == null) {
-			identifier = getIdentifier();
-		}
-		config.setPersistenceDbName(identifier + "-" + config.getPersistenceDbName());
-	}
-
 	@OnScheduled
 	public void onScheduled(final ProcessContext context) {
 		String dataSourceUrl = LdesProcessorProperties.getDataSourceUrl(context);
 		Lang dataSourceFormat = LdesProcessorProperties.getDataSourceFormat(context);
-		Long fragmentExpirationInterval = LdesProcessorProperties.getFragmentExpirationInterval(context);
+		String apiKey = getApiKey(context);
+		RequestExecutor requestExecutor = new DefaultConfig().createRequestExecutor();
+		if (!apiKey.equals("")) {
+			requestExecutor = new ApiKeyConfig(apiKey, getApiKeyHeader(context)).createRequestExecutor();
+		}
 
-		config.setApiKey(getApiKey(context));
-		config.setApiKeyHeader(getApiKeyHeader(context));
+		Ldes ldes = getLdes(dataSourceUrl, dataSourceFormat, requestExecutor);
 
-		ldesService = LdesClientImplFactory.getLdesService(config);
-
-		ldesService.setDataSourceFormat(dataSourceFormat);
-		ldesService.setFragmentExpirationInterval(fragmentExpirationInterval);
-		ldesService.queueFragment(getStartingUrl(dataSourceUrl, dataSourceFormat));
+		treeNodeProcessor = new TreeNodeProcessor(ldes, new SqliteTreeNodeRepository(), new SqliteMemberRepository(),
+				new TreeNodeFetcher(requestExecutor), true);
+		memberSupplier = new MemberSupplier(treeNodeProcessor);
+		determineLdesProperties(ldes, requestExecutor, context);
 
 		LOGGER.info("LDES extraction processor {} with base url {} (expected LDES source format: {})",
 				context.getName(), dataSourceUrl, dataSourceFormat);
 	}
 
-	private String getStartingUrl(String dataSourceUrl, Lang dataSourceFormat) {
-		final ApiKey apiKey = new ApiKey(config.getApiKeyHeader(), config.getApiKey());
-		return endpointRequester
-				.determineStartingNode(new Endpoint(dataSourceUrl, dataSourceFormat, apiKey))
-				.map(StartingNode::url)
-				.orElseThrow(
-						() -> new IllegalArgumentException(
-								"Starting url could not be determined for fragmentId: " + dataSourceUrl));
+	private void determineLdesProperties(Ldes ldes, RequestExecutor requestExecutor, ProcessContext context) {
+		boolean timestampPath = streamTimestampPathProperty(context);
+		boolean versionOfPath = streamVersionOfProperty(context);
+		boolean shape = streamShapeProperty(context);
+		ldesProperties = new LdesPropertiesExtractor(requestExecutor).getLdesProperties(ldes, timestampPath,
+				versionOfPath, shape);
+	}
+
+	private Ldes getLdes(String dataSourceUrl, Lang dataSourceFormat, RequestExecutor requestExecutor) {
+		StartingTreeNodeFinder startingTreeNodeFinder = new StartingTreeNodeFinder(requestExecutor);
+		Optional<TreeNode> startingTreeNode = startingTreeNodeFinder
+				.determineStartingTreeNode(new Endpoint(dataSourceUrl, dataSourceFormat));
+		return new Ldes(startingTreeNode.get().getUrl(), dataSourceFormat);
 	}
 
 	@Override
 	public void onTrigger(ProcessContext context, ProcessSession session) throws ProcessException {
-		if (ldesService.hasFragmentsToProcess()) {
-			Lang dataDestinationFormat = LdesProcessorProperties.getDataDestinationFormat(context);
-			LdesFragment fragment = ldesService.processNextFragment();
+		SuppliedMember memberRecord = memberSupplier.get();
 
-			String timestampPath = streamTimestampPathProperty(context) ? fragment.getTimestampPath()
-					.orElseThrow(() -> new LdesPropertyNotFoundException(LdesFragment.LDES_TIMESTAMP_PATH.toString()))
-					: null;
-			String versionOfPath = streamVersionOfProperty(context) ? fragment.getVersionOfPath()
-					.orElseThrow(() -> new LdesPropertyNotFoundException(LdesFragment.LDES_VERSION_OF.toString()))
-					: null;
-			String shape = streamShapeProperty(context) ? fragment.getShaclShape()
-					.orElseThrow(() -> new LdesPropertyNotFoundException(LdesFragment.TREE_SHAPE.toString()))
-					: null;
+		FlowFile flowFile = session.create();
 
-			// Send the processed members to the next Nifi processor
-			fragment.getMembers()
-					.forEach(ldesMember -> {
-						FlowFile flowFile = session.create();
-
-						if (streamTimestampPathProperty(context)) {
-							session.putAttribute(flowFile, "ldes.timestamppath", timestampPath);
-						}
-						if (streamVersionOfProperty(context)) {
-							session.putAttribute(flowFile, "ldes.isversionofpath", versionOfPath);
-						}
-						if (streamShapeProperty(context)) {
-							session.putAttribute(flowFile, "ldes.shacleshapes", shape);
-						}
-
-						FlowManager.sendRDFToRelation(session, flowFile,
-								ModelConverter.convertModelToString(ldesMember.getMemberModel(), dataDestinationFormat),
-								DATA_RELATIONSHIP, dataDestinationFormat);
-					});
+		if (streamTimestampPathProperty(context)) {
+			session.putAttribute(flowFile, "ldes.timestamppath", ldesProperties.getTimestampPath());
 		}
+		if (streamVersionOfProperty(context)) {
+			session.putAttribute(flowFile, "ldes.isversionofpath", ldesProperties.getVersionOfPath());
+		}
+		if (streamShapeProperty(context)) {
+			session.putAttribute(flowFile, "ldes.shacleshapes", ldesProperties.getShape());
+		}
+		Lang dataDestinationFormat = LdesProcessorProperties.getDataDestinationFormat(context);
+		FlowManager.sendRDFToRelation(session, flowFile,
+				convertModelToString(memberRecord.getModel(), dataDestinationFormat),
+				DATA_RELATIONSHIP, dataDestinationFormat);
 	}
 
 	@OnRemoved
 	public void onRemoved() {
-		ldesService.getStateManager().destroyState();
+		treeNodeProcessor.destoryState();
+	}
+
+	public static String convertModelToString(Model model, Lang dataDestinationFormat) {
+		StringWriter out = new StringWriter();
+
+		RDFDataMgr.write(out, model, dataDestinationFormat);
+
+		return out.toString();
 	}
 
 }
---
+++ ldi-nifi/ldi-nifi-processors/ldes-client-processor/src/main/java/be/vlaanderen/informatievlaanderen/ldes/ldi/services/LdesPropertiesExtractor.java
@@ -0,0 +1,72 @@
+package be.vlaanderen.informatievlaanderen.ldes.ldi.services;
+
+import be.vlaanderen.informatievlaanderen.ldes.client.exceptions.LdesPropertyNotFoundException;
+import be.vlaanderen.informatievlaanderen.ldes.client.valueobjects.LdesFragment;
+import be.vlaanderen.informatievlaanderen.ldes.ldi.domain.valueobjects.LdesProperties;
+import ldes.client.requestexecutor.domain.valueobjects.Request;
+import ldes.client.requestexecutor.domain.valueobjects.Response;
+import ldes.client.requestexecutor.executor.RequestExecutor;
+import ldes.client.treenodefetcher.domain.valueobjects.TreeNodeRequest;
+import ldes.client.treenodesupplier.domain.valueobject.Ldes;
+import org.apache.jena.rdf.model.*;
+import org.apache.jena.riot.RDFParser;
+
+import java.util.Optional;
+
+import static org.apache.jena.rdf.model.ResourceFactory.createProperty;
+
+public class LdesPropertiesExtractor {
+
+	public static final String LDES = "https://w3id.org/ldes#";
+	public static final Property LDES_VERSION_OF = createProperty(LDES, "versionOfPath");
+	public static final Property LDES_TIMESTAMP_PATH = createProperty(LDES, "timestampPath");
+	public static final Property TREE_SHAPE = createProperty("https://w3id.org/tree#", "shape");
+	private final RequestExecutor requestExecutor;
+
+	public LdesPropertiesExtractor(RequestExecutor requestExecutor) {
+		this.requestExecutor = requestExecutor;
+	}
+
+	protected Optional<String> getPropertyValue(Model model, Property property) {
+		return model.listStatements(null, property, (Resource) null)
+				.nextOptional()
+				.map(Statement::getObject)
+				.map(RDFNode::asResource)
+				.map(Resource::toString);
+	}
+
+	public LdesProperties getLdesProperties(Ldes ldes, boolean needTimestampPath, boolean needVersionOfPath,
+			boolean needShape) {
+		TreeNodeRequest request = ldes.createRequest(ldes.getStartingNodeUrl());
+		Request httpRequest = request.createRequest();
+		Response response = requestExecutor.execute(httpRequest);
+		Model model = RDFParser
+				.fromString(response.getBody().orElseThrow())
+				.lang(request.getLang())
+				.build()
+				.toModel();
+		String timestampPath = getProperty(needTimestampPath, getProperty(model), LdesFragment.LDES_TIMESTAMP_PATH);
+		String versionOfPath = getProperty(needVersionOfPath, getVersionOfPath(model), LdesFragment.LDES_VERSION_OF);
+		String shape = getProperty(needShape, getShaclShape(model), LdesFragment.TREE_SHAPE);
+		return new LdesProperties(timestampPath, versionOfPath, shape);
+	}
+
+	private String getProperty(boolean needTimestampPath, Optional<String> model, Property ldesTimestampPath) {
+		return needTimestampPath ? model
+				.orElseThrow(() -> new LdesPropertyNotFoundException(ldesTimestampPath.toString()))
+				: null;
+	}
+
+	public Optional<String> getProperty(Model model) {
+		return getPropertyValue(model, LDES_TIMESTAMP_PATH);
+	}
+
+	public Optional<String> getVersionOfPath(Model model) {
+		return getPropertyValue(model, LDES_VERSION_OF);
+	}
+
+	public Optional<String> getShaclShape(Model model) {
+		return getPropertyValue(model, TREE_SHAPE);
+	}
+
+}
