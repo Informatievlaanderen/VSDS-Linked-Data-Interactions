62a2498de8e496ed98e928d62098c93c72270418
WLefever <wouter.lefever@cegeka.com>
2023-03-13 10:35:48 +0100

    deep copy

---
+++ ldi-core/ldes-client-v2/tree-node-supplier/src/test/java/ldes/client/treenodesupplier/domain/valueobject/SuppliedMemberTest.java
@@ -0,0 +1,20 @@
+package ldes.client.treenodesupplier.domain.valueobject;
+
+import org.apache.jena.rdf.model.Model;
+import org.apache.jena.rdf.model.ModelFactory;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SuppliedMemberTest {
+
+    @Test
+    void when_SuppliedMemberIsCreated_ModelIsDeepCopied() {
+        Model model = ModelFactory.createDefaultModel();
+        SuppliedMember suppliedMember = new SuppliedMember(model);
+
+        model=null;
+        assertNotNull(suppliedMember.getModel());
+    }
+
+}
