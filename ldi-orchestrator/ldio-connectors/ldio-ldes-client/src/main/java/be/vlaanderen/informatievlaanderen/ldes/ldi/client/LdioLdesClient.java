942c79d06f22d58b29524c24b9c5fbbe156e0c0b
tom-desktop-linux <tom.vanbesien@gmail.com>
2023-03-15 12:45:41 +0100

    Throw exception from ldio client executor

--- ldi-orchestrator/ldio-connectors/ldio-ldes-client/src/main/java/be/vlaanderen/informatievlaanderen/ldes/ldi/client/LdioLdesClient.java
+++ ldi-orchestrator/ldio-connectors/ldio-ldes-client/src/main/java/be/vlaanderen/informatievlaanderen/ldes/ldi/client/LdioLdesClient.java
@@ -3,6 +3,7 @@ package be.vlaanderen.informatievlaanderen.ldes.ldi.client;
 import be.vlaanderen.informatievlaanderen.ldes.ldi.services.ComponentExecutor;
 import be.vlaanderen.informatievlaanderen.ldes.ldi.types.LdiInput;
 
+import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 
 import static java.util.concurrent.Executors.newSingleThreadExecutor;
@@ -11,8 +12,16 @@ public class LdioLdesClient extends LdiInput {
 
 	public LdioLdesClient(ComponentExecutor executor, LdesClientRunner ldesClientRunner) {
 		super(executor, null);
-		ExecutorService executorService = newSingleThreadExecutor();
-		executorService.submit(ldesClientRunner);
-		executorService.shutdown();
+		final ExecutorService executorService = newSingleThreadExecutor();
+		try {
+			executorService.submit(ldesClientRunner).get();
+		} catch (InterruptedException e) {
+			Thread.currentThread().interrupt();
+			throw new RuntimeException(e);
+		} catch (ExecutionException e) {
+			throw new RuntimeException(e);
+		} finally {
+			executorService.shutdown();
+		}
 	}
 }
