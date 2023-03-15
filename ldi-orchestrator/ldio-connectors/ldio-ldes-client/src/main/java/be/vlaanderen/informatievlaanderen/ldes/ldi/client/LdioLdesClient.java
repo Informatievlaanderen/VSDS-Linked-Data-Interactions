7fbc2d3b0c012f2d8186d42974975ba64ee7fb13
tom-desktop-linux <tom.vanbesien@gmail.com>
2023-03-15 11:58:03 +0100

    Use autoclose

--- ldi-orchestrator/ldio-connectors/ldio-ldes-client/src/main/java/be/vlaanderen/informatievlaanderen/ldes/ldi/client/LdioLdesClient.java
+++ ldi-orchestrator/ldio-connectors/ldio-ldes-client/src/main/java/be/vlaanderen/informatievlaanderen/ldes/ldi/client/LdioLdesClient.java
@@ -11,8 +11,8 @@ public class LdioLdesClient extends LdiInput {
 
 	public LdioLdesClient(ComponentExecutor executor, LdesClientRunner ldesClientRunner) {
 		super(executor, null);
-		ExecutorService executorService = newSingleThreadExecutor();
-		executorService.submit(ldesClientRunner);
-		executorService.shutdown();
+		try (ExecutorService executorService = newSingleThreadExecutor()) {
+			executorService.submit(ldesClientRunner);
+		}
 	}
 }
