d95edeaee002f500877a7fa5419cc2893abfbe27
tom-desktop-linux <tom.vanbesien@gmail.com>
2023-03-17 13:08:54 +0100

    format

--- ldi-core/ldes-client/request-executor/src/main/java/ldes/client/requestexecutor/domain/valueobjects/executorsupplier/ExponentialRandomBackoffConfig.java
+++ ldi-core/ldes-client/request-executor/src/main/java/ldes/client/requestexecutor/domain/valueobjects/executorsupplier/ExponentialRandomBackoffConfig.java
@@ -7,19 +7,19 @@ import ldes.client.requestexecutor.exceptions.HttpRequestException;
 
 public class ExponentialRandomBackoffConfig {
 
-    private final int maxAttempts;
+	private final int maxAttempts;
 
-    public ExponentialRandomBackoffConfig(int maxAttempts) {
-        this.maxAttempts = maxAttempts;
-    }
+	public ExponentialRandomBackoffConfig(int maxAttempts) {
+		this.maxAttempts = maxAttempts;
+	}
 
-    public RetryConfig createRetryConfig() {
-        return RetryConfig.<Response>custom()
-                .maxAttempts(maxAttempts)
-                .intervalFunction(IntervalFunction.ofExponentialRandomBackoff())
-                .retryOnResult(response -> response == null || response.getHttpStatus() >= 500)
-                .retryOnException(HttpRequestException.class::isInstance)
-                .build();
-    }
+	public RetryConfig createRetryConfig() {
+		return RetryConfig.<Response>custom()
+				.maxAttempts(maxAttempts)
+				.intervalFunction(IntervalFunction.ofExponentialRandomBackoff())
+				.retryOnResult(response -> response == null || response.getHttpStatus() >= 500)
+				.retryOnException(HttpRequestException.class::isInstance)
+				.build();
+	}
 
 }
