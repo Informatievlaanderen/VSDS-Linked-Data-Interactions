ec5687956382529883afcb5cc3a222d51e690980
Jan Robert <jan.robert@cegeka.com>
2023-03-17 14:48:52 +0100

    feat: router funtions

--- ldi-orchestrator/ldio-application/src/main/java/be/vlaanderen/informatievlaanderen/ldes/ldio/Application.java
+++ ldi-orchestrator/ldio-application/src/main/java/be/vlaanderen/informatievlaanderen/ldes/ldio/Application.java
@@ -1,9 +1,16 @@
 package be.vlaanderen.informatievlaanderen.ldes.ldio;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.web.reactive.config.EnableWebFlux;
+
+import java.util.Arrays;
 
 @SpringBootApplication
+@EnableWebFlux
 public class Application {
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
