2269219275b2a1ba6e7c04c3e59c89a690851fc2
Tomvbe <34196062+Tomvbe@users.noreply.github.com>
2023-03-17 11:43:47 +0100

    Give client correct package name (#53)

--- ldi-orchestrator/ldio-connectors/ldio-ldes-client/src/main/java/be/vlaanderen/informatievlaanderen/ldes/ldio/LdioLdesClient.java
+++ ldi-orchestrator/ldio-connectors/ldio-ldes-client/src/main/java/be/vlaanderen/informatievlaanderen/ldes/ldio/LdioLdesClient.java
@@ -1,9 +1,8 @@
-package be.vlaanderen.informatievlaanderen.ldes.ldi.client;
+package be.vlaanderen.informatievlaanderen.ldes.ldio;
 
 import be.vlaanderen.informatievlaanderen.ldes.ldi.services.ComponentExecutor;
 import be.vlaanderen.informatievlaanderen.ldes.ldi.types.LdiInput;
 
-import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 
 import static java.util.concurrent.Executors.newSingleThreadExecutor;
@@ -13,15 +12,6 @@ public class LdioLdesClient extends LdiInput {
 	public LdioLdesClient(ComponentExecutor executor, LdesClientRunner ldesClientRunner) {
 		super(executor, null);
 		final ExecutorService executorService = newSingleThreadExecutor();
-		try {
-			executorService.submit(ldesClientRunner).get();
-		} catch (InterruptedException e) {
-			Thread.currentThread().interrupt();
-			throw new RuntimeException(e);
-		} catch (ExecutionException e) {
-			throw new RuntimeException(e);
-		} finally {
-			executorService.shutdown();
-		}
+		executorService.submit(ldesClientRunner);
 	}
 }
