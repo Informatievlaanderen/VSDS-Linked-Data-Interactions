e5d2acf75658b02292854093c3bbcd4e48d95b5d
tom-desktop-linux <tom.vanbesien@gmail.com>
2023-03-17 12:01:40 +0100

    Implement core

---
+++ ldi-core/ldes-client/request-executor/src/main/java/ldes/client/requestexecutor/domain/valueobjects/executorsupplier/ExponentialRandomBackoffConfig.java
@@ -0,0 +1,25 @@
+package ldes.client.requestexecutor.domain.valueobjects.executorsupplier;
+
+import io.github.resilience4j.core.IntervalFunction;
+import io.github.resilience4j.retry.RetryConfig;
+import ldes.client.requestexecutor.domain.valueobjects.Response;
+import ldes.client.requestexecutor.exceptions.HttpRequestException;
+
+public class ExponentialRandomBackoffConfig {
+
+    private final int maxAttempts;
+
+    public ExponentialRandomBackoffConfig(int maxAttempts) {
+        this.maxAttempts = maxAttempts;
+    }
+
+    public RetryConfig createRetryConfig() {
+        return RetryConfig.<Response>custom()
+                .maxAttempts(maxAttempts)
+                .intervalFunction(IntervalFunction.ofExponentialRandomBackoff())
+                .retryOnResult(response -> response == null || response.getHttpStatus() >= 500)
+                .retryOnException(HttpRequestException.class::isInstance)
+                .build();
+    }
+
+}
