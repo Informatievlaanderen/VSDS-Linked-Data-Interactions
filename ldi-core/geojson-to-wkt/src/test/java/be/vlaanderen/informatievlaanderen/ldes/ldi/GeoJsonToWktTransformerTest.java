635d5867437be73ff62f7dee6efed995f2323880
tom-desktop-linux <tom.vanbesien@gmail.com>
2023-03-22 18:32:38 +0100

    Add some assertions

--- ldi-core/geojson-to-wkt/src/test/java/be/vlaanderen/informatievlaanderen/ldes/ldi/GeoJsonToWktTransformerTest.java
+++ ldi-core/geojson-to-wkt/src/test/java/be/vlaanderen/informatievlaanderen/ldes/ldi/GeoJsonToWktTransformerTest.java
@@ -9,6 +9,8 @@ import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
+import static be.vlaanderen.informatievlaanderen.ldes.ldi.MyWktConverter.COORDINATES;
+import static be.vlaanderen.informatievlaanderen.ldes.ldi.MyWktConverter.GEOMETRY;
 import static org.apache.jena.rdf.model.ResourceFactory.createProperty;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
@@ -31,6 +33,8 @@ class GeoJsonToWktTransformerTest {
 
         assertTrue(resultWkts.contains("POINT (100 0)^^http://www.opengis.net/ont/geosparql#wktLiteral"));
         assertTrue(resultWkts.contains("POINT (101 0)^^http://www.opengis.net/ont/geosparql#wktLiteral"));
+        assertTrue(result.listStatements(null, GEOMETRY, (RDFNode) null).toList().isEmpty());
+        assertTrue(result.listStatements(null, COORDINATES, (RDFNode) null).toList().isEmpty());
     }
 
     @Test
@@ -46,6 +50,8 @@ class GeoJsonToWktTransformerTest {
                 .toList();
 
         assertTrue(resultWkts.contains("POLYGON ((100 0, 101 0, 101 1, 100 1, 100 0), (100.8 0.8, 100.8 0.2, 100.2 0.2, 100.2 0.8, 100.8 0.8), (100.95 0.9, 100.95 0.5, 100.9 0.2, 100.9 0.5, 100.95 0.9))^^http://www.opengis.net/ont/geosparql#wktLiteral"));
+        assertTrue(result.listStatements(null, GEOMETRY, (RDFNode) null).toList().isEmpty());
+        assertTrue(result.listStatements(null, COORDINATES, (RDFNode) null).toList().isEmpty());
     }
 
 }
