e1f86eac3fc822dec284987cb68f48f1d8f83b9a
tom-desktop-linux <tom.vanbesien@gmail.com>
2023-03-22 11:06:02 +0100

    Refactor constants

--- ldi-core/geojson-to-wkt/src/main/java/be/vlaanderen/informatievlaanderen/ldes/ldi/MyWktConverter.java
+++ ldi-core/geojson-to-wkt/src/main/java/be/vlaanderen/informatievlaanderen/ldes/ldi/MyWktConverter.java
@@ -5,6 +5,7 @@ import org.apache.jena.rdf.model.Property;
 import org.apache.jena.rdf.model.RDFNode;
 import org.apache.jena.rdf.model.Resource;
 import org.apache.jena.rdf.model.Statement;
+import org.apache.jena.vocabulary.RDF;
 import org.locationtech.jts.geom.Coordinate;
 import org.locationtech.jts.geom.Geometry;
 import org.locationtech.jts.geom.GeometryFactory;
@@ -22,11 +23,6 @@ import static org.apache.jena.rdf.model.ResourceFactory.createProperty;
 
 public class MyWktConverter {
 
-    public static final String RDF_URI = "http://www.w3.org/1999/02/22-rdf-syntax-ns#";
-    public static final Property RDF_TYPE = createProperty(RDF_URI + "type");
-    public static final Property RDF_FIRST = createProperty(RDF_URI + "first");
-    public static final Property RDF_REST = createProperty(RDF_URI + "rest");
-    public static final Property RDF_NIL = createProperty(RDF_URI + "nil");
     public static final Property GEOMETRY = createProperty("https://purl.org/geojson/vocab#geometry");
     public static final Property COORDINATES = createProperty("https://purl.org/geojson/vocab#coordinates");
 
@@ -80,17 +76,17 @@ public class MyWktConverter {
     }
 
     private Coordinate createPoint(Model model, Resource coordinates) {
-        double first = model.listStatements(coordinates, RDF_FIRST, (RDFNode) null).nextStatement().getObject().asLiteral().getDouble();
-        Resource restId = model.listStatements(coordinates, RDF_REST, (RDFNode) null).nextStatement().getObject().asResource();
-        double second = model.listStatements(restId, RDF_FIRST, (RDFNode) null).nextStatement().getObject().asLiteral().getDouble();
+        double first = model.listStatements(coordinates, RDF.first, (RDFNode) null).nextStatement().getObject().asLiteral().getDouble();
+        Resource restId = model.listStatements(coordinates, RDF.rest, (RDFNode) null).nextStatement().getObject().asResource();
+        double second = model.listStatements(restId, RDF.first, (RDFNode) null).nextStatement().getObject().asLiteral().getDouble();
         return new Coordinate(first, second);
     }
 
     private List<Coordinate> createLineString(Model model, Resource coordinates, List<Coordinate> result) {
-        Resource firstPoint = model.listStatements(coordinates, RDF_FIRST, (RDFNode) null).nextStatement().getObject().asResource();
+        Resource firstPoint = model.listStatements(coordinates, RDF.first, (RDFNode) null).nextStatement().getObject().asResource();
         result.add(createPoint(model, firstPoint));
-        Resource nextPoint = model.listStatements(coordinates, RDF_REST, (RDFNode) null).nextStatement().getObject().asResource();
-        if (RDF_NIL.getURI().equals(nextPoint.getURI())) {
+        Resource nextPoint = model.listStatements(coordinates, RDF.rest, (RDFNode) null).nextStatement().getObject().asResource();
+        if (RDF.nil.getURI().equals(nextPoint.getURI())) {
             return result;
         } else {
             return createLineString(model, nextPoint, result);
@@ -98,11 +94,11 @@ public class MyWktConverter {
     }
 
     private List<List<Coordinate>> createPolygon(Model model, Resource coordinates, List<List<Coordinate>> result) {
-        Resource exteriorRing = model.listStatements(coordinates, RDF_FIRST, (RDFNode) null).nextStatement().getObject().asResource();
+        Resource exteriorRing = model.listStatements(coordinates, RDF.first, (RDFNode) null).nextStatement().getObject().asResource();
         List<Coordinate> exRing = new ArrayList<>();
         result.add(createLineString(model, exteriorRing, exRing));
-        Resource nextRing = model.listStatements(coordinates, RDF_REST, (RDFNode) null).nextStatement().getObject().asResource();
-        if (RDF_NIL.getURI().equals(nextRing.getURI())) {
+        Resource nextRing = model.listStatements(coordinates, RDF.rest, (RDFNode) null).nextStatement().getObject().asResource();
+        if (RDF.nil.getURI().equals(nextRing.getURI())) {
             return result;
         } else {
             return createPolygon(model, nextRing, result);
@@ -110,10 +106,10 @@ public class MyWktConverter {
     }
 
     private List<List<List<Coordinate>>> createMultiPolygon(Model model, Resource coordinates, List<List<List<Coordinate>>> result) {
-        Resource firstPolygon = model.listStatements(coordinates, RDF_FIRST, (RDFNode) null).nextStatement().getObject().asResource();
+        Resource firstPolygon = model.listStatements(coordinates, RDF.first, (RDFNode) null).nextStatement().getObject().asResource();
         result.add(createPolygon(model, firstPolygon, new ArrayList<>()));
-        Resource nextPolygon = model.listStatements(coordinates, RDF_REST, (RDFNode) null).nextStatement().getObject().asResource();
-        if (RDF_NIL.getURI().equals(nextPolygon.getURI())) {
+        Resource nextPolygon = model.listStatements(coordinates, RDF.rest, (RDFNode) null).nextStatement().getObject().asResource();
+        if (RDF.nil.getURI().equals(nextPolygon.getURI())) {
             return result;
         } else {
             return createMultiPolygon(model, nextPolygon, result);
@@ -121,9 +117,9 @@ public class MyWktConverter {
     }
 
     private GeoType getType(Model geojson, Resource geometryId) {
-        final List<Statement> typeList = geojson.listStatements(geometryId, RDF_TYPE, (RDFNode) null).toList();
+        final List<Statement> typeList = geojson.listStatements(geometryId, RDF.type, (RDFNode) null).toList();
         if (typeList.size() != 1) {
-            final String errorMsg = "Could not determine %s of %s".formatted(RDF_TYPE.getURI(), GEOMETRY.getURI());
+            final String errorMsg = "Could not determine %s of %s".formatted(RDF.type.getURI(), GEOMETRY.getURI());
             throw new IllegalArgumentException(errorMsg);
         }
 
