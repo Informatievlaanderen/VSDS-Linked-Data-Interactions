384ec6d92a9bb7b05a85bcc94f5fb243784f33f3
tom-desktop-linux <tom.vanbesien@gmail.com>
2023-03-24 16:41:40 +0100

    implement nifi geojson to wkt

---
+++ ldi-nifi/ldi-nifi-processors/geojson-to-wkt-processor/src/test/java/be/vlaanderen/informatievlaanderen/ldes/ldi/processors/GeoJsonToWktProcessorTest.java
@@ -0,0 +1,51 @@
+package be.vlaanderen.informatievlaanderen.ldes.ldi.processors;
+
+import org.apache.jena.rdf.model.Model;
+import org.apache.jena.riot.Lang;
+import org.apache.jena.riot.RDFParser;
+import org.apache.jena.riot.RDFWriter;
+import org.apache.nifi.util.MockFlowFile;
+import org.apache.nifi.util.TestRunner;
+import org.apache.nifi.util.TestRunners;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static be.vlaanderen.informatievlaanderen.ldes.ldi.processors.config.GeoJsonToWktProcessorProperties.DATA_SOURCE_FORMAT;
+import static be.vlaanderen.informatievlaanderen.ldes.ldi.processors.services.FlowManager.FAILURE;
+import static be.vlaanderen.informatievlaanderen.ldes.ldi.processors.services.FlowManager.SUCCESS;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class GeoJsonToWktProcessorTest {
+
+	private TestRunner testRunner;
+
+	@BeforeEach
+	void init() {
+		testRunner = TestRunners.newTestRunner(GeoJsonToWktProcessor.class);
+	}
+
+	@Test
+	void testProcessor() {
+		Model inputModel = RDFParser.source("geojson-all-types.json").lang(Lang.JSONLD).build().toModel();
+		String inputModelString = RDFWriter.source(inputModel).lang(Lang.JSONLD).build().asString();
+
+		testRunner.setProperty(DATA_SOURCE_FORMAT, Lang.JSONLD.getHeaderString());
+		testRunner.enqueue(inputModelString);
+		testRunner.run();
+
+		MockFlowFile result = testRunner.getFlowFilesForRelationship(SUCCESS).get(0);
+		Model resultModel = RDFParser.fromString(result.getContent()).lang(Lang.JSONLD).build().toModel();
+		Model expectedModel = RDFParser.source("result-all-types.json").lang(Lang.JSONLD).build().toModel();
+		assertTrue(expectedModel.isIsomorphicWith(resultModel));
+	}
+
+	@Test
+	void testFailure() {
+		testRunner.enqueue("random");
+		testRunner.run();
+
+		assertTrue(testRunner.getFlowFilesForRelationship(SUCCESS).isEmpty());
+		assertFalse(testRunner.getFlowFilesForRelationship(FAILURE).isEmpty());
+	}
+}
