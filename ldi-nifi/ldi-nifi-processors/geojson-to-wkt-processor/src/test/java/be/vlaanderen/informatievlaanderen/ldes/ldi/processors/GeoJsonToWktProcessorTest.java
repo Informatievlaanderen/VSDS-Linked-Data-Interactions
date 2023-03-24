0212631028026fc77aa00c049ae690ae3bb847bf
tom-desktop-linux <tom.vanbesien@gmail.com>
2023-03-24 16:26:35 +0100

    Impl nifi

---
+++ ldi-nifi/ldi-nifi-processors/geojson-to-wkt-processor/src/test/java/be/vlaanderen/informatievlaanderen/ldes/ldi/processors/GeoJsonToWktProcessorTest.java
@@ -0,0 +1,8 @@
+package be.vlaanderen.informatievlaanderen.ldes.ldi.processors;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class GeoJsonToWktProcessorTest {
+
+    // TODO: 24/03/2023  
+}
