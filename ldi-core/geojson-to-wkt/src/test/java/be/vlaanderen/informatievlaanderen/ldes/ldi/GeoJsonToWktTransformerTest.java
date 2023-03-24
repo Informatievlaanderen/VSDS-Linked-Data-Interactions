712c150d9ad363301236c412c9e8a909af7134b2
tom-desktop-linux <tom.vanbesien@gmail.com>
2023-03-24 11:23:46 +0100

    test edge case

--- ldi-core/geojson-to-wkt/src/test/java/be/vlaanderen/informatievlaanderen/ldes/ldi/GeoJsonToWktTransformerTest.java
+++ ldi-core/geojson-to-wkt/src/test/java/be/vlaanderen/informatievlaanderen/ldes/ldi/GeoJsonToWktTransformerTest.java
@@ -1,10 +1,17 @@
 package be.vlaanderen.informatievlaanderen.ldes.ldi;
 
-import org.apache.jena.rdf.model.Model;
+import org.apache.jena.rdf.model.*;
 import org.apache.jena.riot.Lang;
 import org.apache.jena.riot.RDFParser;
+import org.apache.jena.vocabulary.RDF;
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+
+import static org.apache.jena.rdf.model.ResourceFactory.createProperty;
+import static org.apache.jena.rdf.model.ResourceFactory.createStatement;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class GeoJsonToWktTransformerTest {
@@ -19,6 +26,20 @@ class GeoJsonToWktTransformerTest {
 		Model expectedResult = RDFParser.source("result-all-types.json").lang(Lang.JSONLD).build().toModel();
 
 		assertTrue(expectedResult.isIsomorphicWith(result));
+	}
+
+	@Test
+	void shouldThrowException_whenGeometryContainsMultipleTypes() {
+		Model model = RDFParser.source("geojson-point.json").lang(Lang.JSONLD).build().toModel();
+		Property pointProperty = createProperty("https://purl.org/geojson/vocab#Point");
+		Resource subject = model.listSubjectsWithProperty(RDF.type, pointProperty).next();
+		Property lineStringProperty = createProperty("https://purl.org/geojson/vocab#LineString");
+		model.add(createStatement(subject, RDF.type, lineStringProperty));
+
+		var exception = assertThrows(IllegalArgumentException.class, () -> transformer.apply(model));
+
+		assertEquals("Could not determine http://www.w3.org/1999/02/22-rdf-syntax-ns#type of " +
+				"https://purl.org/geojson/vocab#geometry", exception.getMessage());
 	}
 
 }
