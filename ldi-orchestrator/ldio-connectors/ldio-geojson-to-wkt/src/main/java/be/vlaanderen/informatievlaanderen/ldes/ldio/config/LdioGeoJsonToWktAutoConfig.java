9a36d525c9ddf604525a7839db9c69b4d5c97e0a
Tomvbe <34196062+Tomvbe@users.noreply.github.com>
2023-03-27 09:29:43 +0200

    Add new geojson component to ldio (#76)

---
+++ ldi-orchestrator/ldio-connectors/ldio-geojson-to-wkt/src/main/java/be/vlaanderen/informatievlaanderen/ldes/ldio/config/LdioGeoJsonToWktAutoConfig.java
@@ -0,0 +1,16 @@
+package be.vlaanderen.informatievlaanderen.ldes.ldio.config;
+
+import be.vlaanderen.informatievlaanderen.ldes.ldi.GeoJsonToWktTransformer;
+import be.vlaanderen.informatievlaanderen.ldes.ldi.config.LdioConfigurator;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class LdioGeoJsonToWktAutoConfig {
+
+	@Bean("be.vlaanderen.informatievlaanderen.ldes.ldi.GeoJsonToWktTransformer")
+	public LdioConfigurator geoJsonToWktConfigurator() {
+		return properties -> new GeoJsonToWktTransformer();
+	}
+
+}
