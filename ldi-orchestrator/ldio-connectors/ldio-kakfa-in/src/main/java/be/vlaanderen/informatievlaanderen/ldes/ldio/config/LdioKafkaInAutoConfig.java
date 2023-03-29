fb7b14fbc61595edef16029bf0f57d9ff1f5d8ed
Jan Robert <jan.robert@cegeka.com>
2023-03-29 16:27:23 +0200

    feat: VSDSPUB-560: kafka in

--- ldi-orchestrator/ldio-connectors/ldio-kakfa-in/src/main/java/be/vlaanderen/informatievlaanderen/ldes/ldio/config/LdioKafkaInAutoConfig.java
+++ ldi-orchestrator/ldio-connectors/ldio-kakfa-in/src/main/java/be/vlaanderen/informatievlaanderen/ldes/ldio/config/LdioKafkaInAutoConfig.java
@@ -57,7 +57,7 @@ public class LdioKafkaInAutoConfig {
 		private Map<String, Object> consumerProps(String bootstrapServers, String groupId, String offsetReset) {
 			Map<String, Object> props = new HashMap<>();
 			props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
-//			props.put(ConsumerConfig.GROUP_ID_CONFIG, groupId);
+			props.put(ConsumerConfig.GROUP_ID_CONFIG, groupId);
 			props.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, offsetReset);
 			props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
 			props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
