4ac2cb5bee26cc2fec7e6e6da1df9290d04ae078
tom-desktop-linux <tom.vanbesien@gmail.com>
2023-03-29 15:20:53 +0200

    Format

--- ldi-orchestrator/ldio-connectors/ldio-kafka-out/src/main/java/be/vlaanderen/informatievlaanderen/ldes/ldio/config/LdioKafkaOutAutoConfig.java
+++ ldi-orchestrator/ldio-connectors/ldio-kafka-out/src/main/java/be/vlaanderen/informatievlaanderen/ldes/ldio/config/LdioKafkaOutAutoConfig.java
@@ -2,9 +2,11 @@ package be.vlaanderen.informatievlaanderen.ldes.ldio.config;
 
 import be.vlaanderen.informatievlaanderen.ldes.ldi.config.ComponentProperties;
 import be.vlaanderen.informatievlaanderen.ldes.ldi.config.LdioConfigurator;
-import be.vlaanderen.informatievlaanderen.ldes.ldio.LdioKafkaOut;
+import be.vlaanderen.informatievlaanderen.ldes.ldi.types.LdiOutput;
+import org.apache.jena.rdf.model.Model;
 import org.apache.jena.riot.Lang;
 import org.apache.jena.riot.RDFLanguages;
+import org.apache.jena.riot.RDFWriter;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.context.annotation.Bean;
@@ -13,36 +15,37 @@ import org.springframework.kafka.core.DefaultKafkaProducerFactory;
 import org.springframework.kafka.core.KafkaTemplate;
 
 import java.util.HashMap;
-import java.util.Map;
 
 @Configuration
 public class LdioKafkaOutAutoConfig {
 
-    private static final Lang DEFAULT_OUTPUT_LANG = Lang.NQUADS;
+	@Bean("be.vlaanderen.informatievlaanderen.ldes.ldio.LdioKafkaOut")
+	public LdioConfigurator ldiKafkaOutConfigurator() {
+		return config -> {
+			final Lang lang = getLang(config);
+			final String topic = config.getProperty("topic-name");
+			final var kafkaTemplate = createKafkaTemplate(config.getProperty("bootstrap-server"));
+			return (LdiOutput) model -> kafkaTemplate.send(topic, toString(lang, model));
+		};
+	}
 
-    @Bean("be.vlaanderen.informatievlaanderen.ldes.ldio.LdioKafkaOut")
-    public LdioConfigurator ldiHttpOutConfigurator() {
-        return config -> {
-            final Lang lang = getLang(config);
-            final String topicName = config.getProperty("topic-name");
-            final String bootstrapServer = config.getProperty("bootstrap-server");
-            return new LdioKafkaOut(createKafkaTemplate(bootstrapServer), lang, topicName);
-        };
-    }
+	private String toString(Lang lang, Model model) {
+		return RDFWriter.source(model).lang(lang).build().asString();
+	}
 
-    private static Lang getLang(ComponentProperties config) {
-        return config
-                .getOptionalProperty("content-type")
-                .map(RDFLanguages::contentTypeToLang)
-                .orElse(DEFAULT_OUTPUT_LANG);
-    }
+	private Lang getLang(ComponentProperties config) {
+		return config
+				.getOptionalProperty("content-type")
+				.map(RDFLanguages::contentTypeToLang)
+				.orElse(Lang.NQUADS);
+	}
 
-    private KafkaTemplate<String, String> createKafkaTemplate(String bootstrapServer) {
-        Map<String, Object> configProps = new HashMap<>();
-        configProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServer);
-        configProps.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
-        configProps.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
-        return new KafkaTemplate<>(new DefaultKafkaProducerFactory<>(configProps));
-    }
+	private KafkaTemplate<String, String> createKafkaTemplate(String bootstrapServer) {
+		final var configProps = new HashMap<String, Object>();
+		configProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServer);
+		configProps.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+		configProps.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+		return new KafkaTemplate<>(new DefaultKafkaProducerFactory<>(configProps));
+	}
 
 }
