a9d7dee504e5187c18c452da38a5c877be0fbb23
tom-desktop-linux <tom.vanbesien@gmail.com>
2023-03-30 09:56:07 +0200

    Implement basic integration test

--- ldi-orchestrator/ldio-integration/src/test/java/be/vlaanderen/informatievlaanderen/ldes/KafkaIntegrationTest.java
+++ ldi-orchestrator/ldio-integration/src/test/java/be/vlaanderen/informatievlaanderen/ldes/KafkaIntegrationTest.java
@@ -8,6 +8,7 @@ import be.vlaanderen.informatievlaanderen.ldes.ldio.config.LdioKafkaInAutoConfig
 import be.vlaanderen.informatievlaanderen.ldes.ldio.config.LdioKafkaOutAutoConfig;
 import org.apache.jena.rdf.model.Model;
 import org.apache.jena.rdf.model.ModelFactory;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.kafka.listener.KafkaMessageListenerContainer;
 import org.springframework.kafka.test.context.EmbeddedKafka;
@@ -19,71 +20,64 @@ import java.util.Map;
 
 import static org.apache.jena.rdf.model.ResourceFactory.createProperty;
 import static org.apache.jena.rdf.model.ResourceFactory.createResource;
-import static org.apache.jena.rdf.model.ResourceFactory.createStatement;
-import static org.apache.jena.rdf.model.ResourceFactory.createStringLiteral;
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.awaitility.Awaitility.await;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @EmbeddedKafka(brokerProperties = { "listeners=PLAINTEXT://localhost:9095", "port=9095" })
 public class KafkaIntegrationTest {
 
-
     final LdioKafkaOutAutoConfig autoConfig = new LdioKafkaOutAutoConfig();
 
-    private String topic = "embedded-test-topic";
+    private final String topic = "embedded-test-topic";
+    private final String bootstrapServer = "localhost:9095";
+    private final String contentType = "text/turtle";
+
+    private List<Model> result;
+
+    @BeforeEach
+    void setUp() {
+        result = new ArrayList<>();
+    }
 
     @Test
-    public void givenEmbeddedKafkaBroker_whenSendingWithSimpleProducer_thenMessageReceived()
-            throws Exception {
+    public void givenEmbeddedKafkaBroker_whenSendingWithSimpleProducer_thenMessageReceived() {
+        var kafkaListener = createKafkaListener();
+        kafkaListener.start();
 
-//        content-type: text/turtle
-//        bootstrap-server: localhost:9092
-//        topic-name: quickstart-events-2
+        var model = createModelWithStatement();
 
-        createConsumer("localhost:9095");
-
-        Map<String, String> config = new HashMap<>();
-        config.put("content-type", "text/turtle");
-        config.put("bootstrap-server", "localhost:9095");
-        config.put("topic-name", topic);
-        ComponentProperties componentProperties = new ComponentProperties(config);
-        LdiOutput output = (LdiOutput) autoConfig.ldiKafkaOutConfigurator().configure(componentProperties);
-
-
-        Model model = ModelFactory.createDefaultModel();
-        model.add(createResource("http://data-from-source/"), createProperty("http://test/"), "Data!");
-
+        var output = createKafkaOutput();
         output.accept(model);
 
-        Thread.sleep(1000);
-
-        assertEquals(1, result.size());
+        await().until(() -> result.size() == 1);
         assertTrue(model.isIsomorphicWith(result.get(0)));
     }
 
-    List<Model> result = new ArrayList<>();
+    private Model createModelWithStatement() {
+        Model model = ModelFactory.createDefaultModel();
+        model.add(createResource("http://data-from-source/"), createProperty("http://test/"), "Data!");
+        return model;
+    }
 
-    private void createConsumer(String s) {
-        final LdioKafkaInAutoConfig autoConfig = new LdioKafkaInAutoConfig();
-        ComponentExecutor componentExecutor = new ComponentExecutor(){
-            @Override
-            public void transformLinkedData(Model linkedDataModel) {
-                result.add(linkedDataModel);
-            }
-        };
-        Map<String, String> config = new HashMap<>();
-        config.put("content-type", "text/turtle");
-        config.put("bootstrap.servers", "localhost:9095");
+    private LdiOutput createKafkaOutput() {
+        final Map<String, String> config = new HashMap<>();
+        config.put("content-type", contentType);
+        config.put("bootstrap-server", bootstrapServer);
+        config.put("topic-name", topic);
+        ComponentProperties componentProperties = new ComponentProperties(config);
+        return (LdiOutput) autoConfig.ldiKafkaOutConfigurator().configure(componentProperties);
+    }
+
+    @SuppressWarnings("unchecked")
+    private KafkaMessageListenerContainer<String, String> createKafkaListener() {
+        final Map<String, String> config = new HashMap<>();
+        config.put("content-type", contentType);
+        config.put("bootstrap.servers", bootstrapServer);
         config.put("topic", topic);
         ComponentProperties componentProperties = new ComponentProperties(config);
-
-
-        @SuppressWarnings("unchecked") var container = (KafkaMessageListenerContainer<String, String>)
+        final ComponentExecutor componentExecutor = model -> result.add(model);
+        return (KafkaMessageListenerContainer<String, String>)
                 new LdioKafkaInAutoConfig().ldioConfigurator().configure(new RdfAdapter(), componentExecutor, componentProperties);
-
-        System.out.println(container.isContainerPaused());
-        container.start();
-        System.out.println(container.isContainerPaused());
     }
 
 }
