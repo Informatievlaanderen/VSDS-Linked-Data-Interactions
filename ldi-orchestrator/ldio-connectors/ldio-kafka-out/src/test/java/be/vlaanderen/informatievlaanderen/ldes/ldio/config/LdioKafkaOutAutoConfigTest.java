44bb8df9ad67e84beee5865a3acd10d3762af8f2
tom-desktop-linux <tom.vanbesien@gmail.com>
2023-04-03 17:42:57 +0200

    add todos

---
+++ ldi-orchestrator/ldio-connectors/ldio-kafka-out/src/test/java/be/vlaanderen/informatievlaanderen/ldes/ldio/config/LdioKafkaOutAutoConfigTest.java
@@ -0,0 +1,13 @@
+package be.vlaanderen.informatievlaanderen.ldes.ldio.config;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class LdioKafkaOutAutoConfigTest {
+
+
+    // TODO: 3/04/2023 exception testen en sasl config
+    // TODO: 3/04/2023 sasl config kafka in testen
+    // TODO: 3/04/2023 readme van kafka in
+    // TODO: 3/04/2023 kijk om config meer ddd aan te pakken
+
+}
