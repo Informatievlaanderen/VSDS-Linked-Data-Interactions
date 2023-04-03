74e71293106d264a12cde27925ed7a8c648fa3a4
Tomvbe <34196062+Tomvbe@users.noreply.github.com>
2023-04-03 11:45:00 +0200

    Implement kafka in sasl plain security (#103)

--- ldi-orchestrator/ldio-connectors/ldio-kakfa-in/src/main/java/be/vlaanderen/informatievlaanderen/ldes/ldio/config/KafkaInConfigKeys.java
+++ ldi-orchestrator/ldio-connectors/ldio-kakfa-in/src/main/java/be/vlaanderen/informatievlaanderen/ldes/ldio/config/KafkaInConfigKeys.java
@@ -10,5 +10,9 @@ public class KafkaInConfigKeys {
 	public static final String CONTENT_TYPE = "content-type";
 	public static final String GROUP_ID = "group-id";
 	public static final String AUTO_OFFSET_RESET = "auto-offset-reset";
+	public static final String SECURITY_PROTOCOL = "security-protocol";
+
+	public static final String SASL_JAAS_USER = "sasl-jaas-user";
+	public static final String SASL_JAAS_PASSWORD = "sasl-jaas-password";
 
 }
---
+++ ldi-orchestrator/ldio-connectors/ldio-kakfa-in/src/test/java/be/vlaanderen/informatievlaanderen/ldes/ldio/config/LdioKafkaInAutoConfigTest.java
@@ -0,0 +1,46 @@
+package be.vlaanderen.informatievlaanderen.ldes.ldio.config;
+
+import be.vlaanderen.informatievlaanderen.ldes.ldi.config.ComponentProperties;
+import org.junit.jupiter.api.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class LdioKafkaInAutoConfigTest {
+
+	@Test
+	void shouldThrowExceptionWhenInvalidAuthConfig() {
+		var configurator = new LdioKafkaInAutoConfig.LdioKafkaInConfigurator();
+
+		Map<String, String> config = getBasicConfig();
+		config.put(KafkaInConfigKeys.SECURITY_PROTOCOL, "Fantasy protocol");
+		ComponentProperties componentProperties = new ComponentProperties(config);
+
+		IllegalArgumentException exception = assertThrows(IllegalArgumentException.class,
+				() -> configurator.configure(null, null, componentProperties));
+
+		assertEquals("Invalid 'security-protocol', the supported protocols are: [NO_AUTH, SASL_SSL_PLAIN]",
+				exception.getMessage());
+	}
+
+	@Test
+	void shouldNotThrowExceptionWhenNoAuthConfig() {
+		var configurator = new LdioKafkaInAutoConfig.LdioKafkaInConfigurator();
+
+		Map<String, String> config = getBasicConfig();
+
+		assertDoesNotThrow(() -> configurator.configure(null, null, new ComponentProperties(config)));
+	}
+
+	private Map<String, String> getBasicConfig() {
+		Map<String, String> config = new HashMap<>();
+		config.put(KafkaInConfigKeys.BOOTSTRAP_SERVERS, "servers");
+		config.put(KafkaInConfigKeys.TOPICS, "topic1");
+		config.put("orchestrator.name", "orchestrator.name");
+		config.put("pipeline.name", "pipeline.name");
+		return config;
+	}
+
+}
