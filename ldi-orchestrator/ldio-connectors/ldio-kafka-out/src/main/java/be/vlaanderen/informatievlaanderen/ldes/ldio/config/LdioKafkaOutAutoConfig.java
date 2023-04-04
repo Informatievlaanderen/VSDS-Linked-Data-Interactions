d31106e33fc04aa56f6220ebd5781f88e9963868
Tom Van Besien <tom.vanbesien@gmail.com>
2023-04-04 11:24:30 +0200

    Refactor kafka out

--- ldi-orchestrator/ldio-connectors/ldio-kafka-out/src/main/java/be/vlaanderen/informatievlaanderen/ldes/ldio/config/LdioKafkaOutAutoConfig.java
+++ ldi-orchestrator/ldio-connectors/ldio-kafka-out/src/main/java/be/vlaanderen/informatievlaanderen/ldes/ldio/config/LdioKafkaOutAutoConfig.java
@@ -1,96 +1,15 @@
 package be.vlaanderen.informatievlaanderen.ldes.ldio.config;
 
-import be.vlaanderen.informatievlaanderen.ldes.ldi.config.ComponentProperties;
 import be.vlaanderen.informatievlaanderen.ldes.ldi.config.LdioConfigurator;
-import be.vlaanderen.informatievlaanderen.ldes.ldi.types.LdiOutput;
-import org.apache.jena.rdf.model.Model;
-import org.apache.jena.riot.Lang;
-import org.apache.jena.riot.RDFLanguages;
-import org.apache.jena.riot.RDFWriter;
-import org.apache.kafka.clients.CommonClientConfigs;
-import org.apache.kafka.clients.producer.ProducerConfig;
-import org.apache.kafka.clients.producer.ProducerRecord;
-import org.apache.kafka.common.config.SaslConfigs;
-import org.apache.kafka.common.header.internals.RecordHeaders;
-import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.kafka.core.DefaultKafkaProducerFactory;
-import org.springframework.kafka.core.KafkaTemplate;
-
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Map;
-
-import static be.vlaanderen.informatievlaanderen.ldes.ldio.config.KafkaOutAuthStrategy.NO_AUTH;
-import static be.vlaanderen.informatievlaanderen.ldes.ldio.config.KafkaOutAuthStrategy.SASL_SSL_PLAIN;
-import static be.vlaanderen.informatievlaanderen.ldes.ldio.config.KafkaOutConfigKeys.BOOTSTRAP_SERVERS;
-import static be.vlaanderen.informatievlaanderen.ldes.ldio.config.KafkaOutConfigKeys.CONTENT_TYPE;
-import static be.vlaanderen.informatievlaanderen.ldes.ldio.config.KafkaOutConfigKeys.SASL_JAAS_PASSWORD;
-import static be.vlaanderen.informatievlaanderen.ldes.ldio.config.KafkaOutConfigKeys.SASL_JAAS_USER;
-import static be.vlaanderen.informatievlaanderen.ldes.ldio.config.KafkaOutConfigKeys.SECURITY_PROTOCOL;
-import static be.vlaanderen.informatievlaanderen.ldes.ldio.config.KafkaOutConfigKeys.TOPIC;
 
 @Configuration
 public class LdioKafkaOutAutoConfig {
 
 	@Bean("be.vlaanderen.informatievlaanderen.ldes.ldio.LdioKafkaOut")
 	public LdioConfigurator ldiKafkaOutConfigurator() {
-		return config -> {
-			final Lang lang = getLang(config);
-			final String topic = config.getProperty(TOPIC);
-			final var kafkaTemplate = createKafkaTemplate(config);
-			return (LdiOutput) model -> kafkaTemplate.send(createProducerRecord(lang, topic, model));
-		};
-	}
-
-	private ProducerRecord<String, String> createProducerRecord(Lang lang, String topic, Model model) {
-		final String message = toString(lang, model);
-		final var headers = new RecordHeaders().add(CONTENT_TYPE, lang.getHeaderString().getBytes());
-		return new ProducerRecord<>(topic, null, (String) null, message, headers);
-	}
-
-	private String toString(Lang lang, Model model) {
-		return RDFWriter.source(model).lang(lang).build().asString();
-	}
-
-	private Lang getLang(ComponentProperties config) {
-		return config
-				.getOptionalProperty(CONTENT_TYPE)
-				.map(RDFLanguages::contentTypeToLang)
-				.orElse(Lang.NQUADS);
-	}
-
-	private KafkaTemplate<String, String> createKafkaTemplate(ComponentProperties config) {
-		final var configProps = new HashMap<String, Object>();
-		configProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, config.getProperty(BOOTSTRAP_SERVERS));
-		configProps.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
-		configProps.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
-
-		var authStrategy = KafkaOutAuthStrategy.from(config.getOptionalProperty(SECURITY_PROTOCOL)
-				.orElse(NO_AUTH.name())).orElseThrow(this::securityProtocolNotSupported);
-		if (SASL_SSL_PLAIN.equals(authStrategy)) {
-			configProps.putAll(createSaslSslPlainConfig(config));
-		}
-
-		return new KafkaTemplate<>(new DefaultKafkaProducerFactory<>(configProps));
-	}
-
-	private IllegalArgumentException securityProtocolNotSupported() {
-		return new IllegalArgumentException("Invalid '%s', the supported protocols are: %s".formatted(
-				SECURITY_PROTOCOL,
-				Arrays.stream(KafkaOutAuthStrategy.values()).map(Enum::name).toList()));
-	}
-
-	private Map<String, ?> createSaslSslPlainConfig(ComponentProperties config) {
-		final Map<String, Object> properties = new HashMap<>();
-		properties.put(CommonClientConfigs.SECURITY_PROTOCOL_CONFIG, "SASL_SSL");
-		properties.put(SaslConfigs.SASL_MECHANISM, "PLAIN");
-		String plainLoginString = ("org.apache.kafka.common.security.plain.PlainLoginModule" +
-				" required username='%s' password='%s';")
-				.formatted(config.getProperty(SASL_JAAS_USER), config.getProperty(SASL_JAAS_PASSWORD));
-		properties.put(SaslConfigs.SASL_JAAS_CONFIG, plainLoginString);
-		return properties;
+		return new LdioKafkaOutConfigurator();
 	}
 
 }
---
+++ ldi-orchestrator/ldio-connectors/ldio-kafka-out/src/main/java/be/vlaanderen/informatievlaanderen/ldes/ldio/config/LdioKafkaOutConfigurator.java
@@ -0,0 +1,74 @@
+package be.vlaanderen.informatievlaanderen.ldes.ldio.config;
+
+import be.vlaanderen.informatievlaanderen.ldes.ldi.config.ComponentProperties;
+import be.vlaanderen.informatievlaanderen.ldes.ldi.config.LdioConfigurator;
+import be.vlaanderen.informatievlaanderen.ldes.ldi.types.LdiComponent;
+import be.vlaanderen.informatievlaanderen.ldes.ldio.LdioKafkaOut;
+import org.apache.jena.riot.Lang;
+import org.apache.jena.riot.RDFLanguages;
+import org.apache.kafka.clients.CommonClientConfigs;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.config.SaslConfigs;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.springframework.kafka.core.DefaultKafkaProducerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+import static be.vlaanderen.informatievlaanderen.ldes.ldio.config.KafkaOutAuthStrategy.NO_AUTH;
+import static be.vlaanderen.informatievlaanderen.ldes.ldio.config.KafkaOutAuthStrategy.SASL_SSL_PLAIN;
+import static be.vlaanderen.informatievlaanderen.ldes.ldio.config.KafkaOutConfigKeys.*;
+import static be.vlaanderen.informatievlaanderen.ldes.ldio.config.KafkaOutConfigKeys.SASL_JAAS_PASSWORD;
+
+public class LdioKafkaOutConfigurator implements LdioConfigurator {
+
+	@Override
+	public LdiComponent configure(ComponentProperties config) {
+		final Lang lang = getLang(config);
+		final String topic = config.getProperty(TOPIC);
+		final var kafkaTemplate = createKafkaTemplate(config);
+		return new LdioKafkaOut(kafkaTemplate, lang, topic);
+	}
+
+	private Lang getLang(ComponentProperties config) {
+		return config
+				.getOptionalProperty(CONTENT_TYPE)
+				.map(RDFLanguages::contentTypeToLang)
+				.orElse(Lang.NQUADS);
+	}
+
+	private KafkaTemplate<String, String> createKafkaTemplate(ComponentProperties config) {
+		final var configProps = new HashMap<String, Object>();
+		configProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, config.getProperty(BOOTSTRAP_SERVERS));
+		configProps.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+		configProps.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+
+		var authStrategy = KafkaOutAuthStrategy.from(config.getOptionalProperty(SECURITY_PROTOCOL)
+				.orElse(NO_AUTH.name())).orElseThrow(this::securityProtocolNotSupported);
+		if (SASL_SSL_PLAIN.equals(authStrategy)) {
+			configProps.putAll(createSaslSslPlainConfig(config));
+		}
+
+		return new KafkaTemplate<>(new DefaultKafkaProducerFactory<>(configProps));
+	}
+
+	private IllegalArgumentException securityProtocolNotSupported() {
+		return new IllegalArgumentException("Invalid '%s', the supported protocols are: %s".formatted(
+				SECURITY_PROTOCOL,
+				Arrays.stream(KafkaOutAuthStrategy.values()).map(Enum::name).toList()));
+	}
+
+	private Map<String, ?> createSaslSslPlainConfig(ComponentProperties config) {
+		final Map<String, Object> properties = new HashMap<>();
+		properties.put(CommonClientConfigs.SECURITY_PROTOCOL_CONFIG, "SASL_SSL");
+		properties.put(SaslConfigs.SASL_MECHANISM, "PLAIN");
+		String plainLoginString = ("org.apache.kafka.common.security.plain.PlainLoginModule" +
+				" required username='%s' password='%s';")
+				.formatted(config.getProperty(SASL_JAAS_USER), config.getProperty(SASL_JAAS_PASSWORD));
+		properties.put(SaslConfigs.SASL_JAAS_CONFIG, plainLoginString);
+		return properties;
+	}
+
+}
