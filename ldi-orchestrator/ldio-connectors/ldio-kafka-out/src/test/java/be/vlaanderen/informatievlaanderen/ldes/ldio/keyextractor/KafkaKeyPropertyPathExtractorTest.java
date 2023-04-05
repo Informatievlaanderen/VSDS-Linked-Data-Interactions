68ac468af2ef183b00795b83ebb108dd9fc61f84
tom-desktop-linux <tom.vanbesien@gmail.com>
2023-04-05 16:19:25 +0200

    Fix codesmell

--- ldi-orchestrator/ldio-connectors/ldio-kafka-out/src/test/java/be/vlaanderen/informatievlaanderen/ldes/ldio/keyextractor/KafkaKeyPropertyPathExtractorTest.java
+++ ldi-orchestrator/ldio-connectors/ldio-kafka-out/src/test/java/be/vlaanderen/informatievlaanderen/ldes/ldio/keyextractor/KafkaKeyPropertyPathExtractorTest.java
@@ -3,85 +3,64 @@ package be.vlaanderen.informatievlaanderen.ldes.ldio.keyextractor;
 import org.apache.jena.rdf.model.Model;
 import org.apache.jena.riot.Lang;
 import org.apache.jena.riot.RDFParser;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtensionContext;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.ArgumentsProvider;
+import org.junit.jupiter.params.provider.ArgumentsSource;
+
+import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNull;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 class KafkaKeyPropertyPathExtractorTest {
 
-	@Test
-	void shouldReturnLiteralStringWhenLinkedPath() {
-		String modelString = """
-				<https://example.com/hindrances/29797> <https://data.com/ns/mobiliteit#zone> <https://example.com/hindrances/zones/a> .
-				<https://example.com/hindrances/zones/a> <https://data.com/ns/mobiliteit#Zone.type> 'my-zone-type' .
-				""";
-		Model model = RDFParser.fromString(modelString).lang(Lang.NQUADS).build().toModel();
-
-		KafkaKeyPropertyPathExtractor extractor = new KafkaKeyPropertyPathExtractor(
-				"<https://data.com/ns/mobiliteit#zone>/<https://data.com/ns/mobiliteit#Zone.type>");
-
-		String result = extractor.getKey(model);
-
-		assertEquals("my-zone-type", result);
+	@ParameterizedTest
+	@ArgumentsSource(PropertyPathExtractorProvider.class)
+	void testGetKey(String testName, String input, String propertyPath, String result) {
+		assertNotNull(testName);
+		Model model = RDFParser.fromString(input).lang(Lang.NQUADS).build().toModel();
+		KafkaKeyPropertyPathExtractor extractor = new KafkaKeyPropertyPathExtractor(propertyPath);
+		assertEquals(result, extractor.getKey(model));
 	}
 
-	@Test
-	void shouldReturnLiteralStringWhenSimplePath() {
-		String modelString = """
-				<https://example.com/hindrances/29797> <https://data.com/ns/mobiliteit#zone> 'my-zone-type' .
-				""";
-		Model model = RDFParser.fromString(modelString).lang(Lang.NQUADS).build().toModel();
-
-		KafkaKeyPropertyPathExtractor extractor = new KafkaKeyPropertyPathExtractor(
-				"<https://data.com/ns/mobiliteit#zone>");
-
-		String result = extractor.getKey(model);
-
-		assertEquals("my-zone-type", result);
+	static class PropertyPathExtractorProvider implements ArgumentsProvider {
+		@Override
+		public Stream<? extends Arguments> provideArguments(ExtensionContext context) {
+			return Stream.of(
+					Arguments.of(
+							"shouldReturnLiteralStringWhenLinkedPath",
+							"""
+										<https://example.com/hindrances/29797> <https://data.com/ns/mobiliteit#zone> <https://example.com/hindrances/zones/a> .
+										<https://example.com/hindrances/zones/a> <https://data.com/ns/mobiliteit#Zone.type> 'my-zone-type' .
+									""",
+							"<https://data.com/ns/mobiliteit#zone>/<https://data.com/ns/mobiliteit#Zone.type>",
+							"my-zone-type"),
+					Arguments.of(
+							"shouldReturnAnyResultStringWhenMultipleResults",
+							"""
+										<https://example.com/hindrances/29797> <https://data.com/ns/mobiliteit#zone> <https://example.com/hindrances/zones/a> .
+										<https://example.com/hindrances/zones/a> <https://data.com/ns/mobiliteit#Zone.type> 'my-zone-type' .
+									""",
+							"<https://data.com/ns/mobiliteit#zone>/<https://data.com/ns/mobiliteit#Zone.type>",
+							"my-zone-type"),
+					Arguments.of(
+							"shouldReturnUriAsStringWhenObjectIsResource",
+							"<https://example.com/hindrances/29797> <https://data.com/ns/mobiliteit#zone> <https://example.com/hindrances/zones/a> .",
+							"<https://data.com/ns/mobiliteit#zone>",
+							"https://example.com/hindrances/zones/a"),
+					Arguments.of(
+							"shouldReturnNullIfPathIsNotFound",
+							"<https://example.com/hindrances/29797> <https://data.com/ns/mobiliteit#zone> <https://example.com/hindrances/zones/a> .",
+							"<https://not-existing>",
+							null),
+					Arguments.of(
+							"shouldReturnLiteralStringWhenSimplePath",
+							"<https://example.com/hindrances/29797> <https://data.com/ns/mobiliteit#zone> 'my-zone-type' .",
+							"<https://data.com/ns/mobiliteit#zone>",
+							"my-zone-type"));
+		}
 	}
 
-	@Test
-	void shouldReturnAnyResultStringWhenMultipleResults() {
-		String modelString = """
-				<https://example.com/hindrances/29797> <https://data.com/ns/mobiliteit#zone> 'my-zone-type' .
-				<https://example.com/hindrances/29797> <https://data.com/ns/mobiliteit#zone> 'other-zone-type' .
-				""";
-		Model model = RDFParser.fromString(modelString).lang(Lang.NQUADS).build().toModel();
-
-		KafkaKeyPropertyPathExtractor extractor = new KafkaKeyPropertyPathExtractor(
-				"<https://data.com/ns/mobiliteit#zone>");
-
-		String result = extractor.getKey(model);
-
-		assertTrue(result.contains("-zone-type"));
-	}
-
-	@Test
-	void shouldReturnUriAsStringWhenObjectIsResource() {
-		String modelString = """
-				<https://example.com/hindrances/29797> <https://data.com/ns/mobiliteit#zone> <https://example.com/hindrances/zones/a> .
-				""";
-		Model model = RDFParser.fromString(modelString).lang(Lang.NQUADS).build().toModel();
-
-		KafkaKeyPropertyPathExtractor extractor = new KafkaKeyPropertyPathExtractor(
-				"<https://data.com/ns/mobiliteit#zone>");
-
-		String result = extractor.getKey(model);
-
-		assertEquals("https://example.com/hindrances/zones/a", result);
-	}
-
-	@Test
-	void shouldReturnNullIfPathIsNotFound() {
-		String modelString = """
-				<https://example.com/hindrances/29797> <https://data.com/ns/mobiliteit#zone> <https://example.com/hindrances/zones/a> .
-				""";
-		Model model = RDFParser.fromString(modelString).lang(Lang.NQUADS).build().toModel();
-
-		KafkaKeyPropertyPathExtractor extractor = new KafkaKeyPropertyPathExtractor("<https://not-existing>");
-
-		assertNull(extractor.getKey(model));
-	}
 }
