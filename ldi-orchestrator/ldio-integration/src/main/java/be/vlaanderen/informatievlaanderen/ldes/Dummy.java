6365c3a0dcdb760eec4608c8e4bccd9c12d4c48f
tom-desktop-linux <tom.vanbesien@gmail.com>
2023-04-05 16:19:35 +0200

    remove dummy

--- ldi-orchestrator/ldio-integration/src/main/java/be/vlaanderen/informatievlaanderen/ldes/Dummy.java
+++
@@ -1,6 +0,0 @@
-package be.vlaanderen.informatievlaanderen.ldes;
-
-public interface Dummy {
-	// We need at least one class to ensure that our Integration tests are included
-	// in the coverage.
-}
