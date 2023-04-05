20040444bc27192a1ba7b023289e86e50c62c9b8
tom-desktop-linux <tom.vanbesien@gmail.com>
2023-04-05 15:53:38 +0200

    format

--- ldi-orchestrator/ldio-integration/src/main/java/be/vlaanderen/informatievlaanderen/ldes/Dummy.java
+++ ldi-orchestrator/ldio-integration/src/main/java/be/vlaanderen/informatievlaanderen/ldes/Dummy.java
@@ -1,6 +1,6 @@
 package be.vlaanderen.informatievlaanderen.ldes;
 
 public interface Dummy {
-    // We need at least one class to ensure that our Integration tests are included
-    // in the coverage.
+	// We need at least one class to ensure that our Integration tests are included
+	// in the coverage.
 }
