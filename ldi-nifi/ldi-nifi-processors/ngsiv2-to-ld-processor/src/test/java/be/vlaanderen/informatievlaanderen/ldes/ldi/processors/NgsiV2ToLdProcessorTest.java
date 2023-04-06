f49a9d7dc1cf76d720494737c1aeb17e0fc9d113
pj-cegeka <119848850+pj-cegeka@users.noreply.github.com>
2023-04-06 09:32:32 +0200

    feat/ added error logging in nifi component (#117)

--- ldi-nifi/ldi-nifi-processors/ngsiv2-to-ld-processor/src/test/java/be/vlaanderen/informatievlaanderen/ldes/ldi/processors/NgsiV2ToLdProcessorTest.java
+++ ldi-nifi/ldi-nifi-processors/ngsiv2-to-ld-processor/src/test/java/be/vlaanderen/informatievlaanderen/ldes/ldi/processors/NgsiV2ToLdProcessorTest.java
@@ -12,17 +12,19 @@ import java.util.Map;
 
 import com.github.tomakehurst.wiremock.junit5.WireMockTest;
 
+import static be.vlaanderen.informatievlaanderen.ldes.ldi.processors.services.FlowManager.FAILURE;
+
 @WireMockTest(httpPort = 10101)
 class NgsiV2ToLdProcessorTest {
 	private final TestRunner testRunner = TestRunners.newTestRunner(NgsiV2ToLdProcessor.class);
 
 	@Test
 	void when_translatingNgsiV2ToLD_expectSuccess() throws IOException {
-		String DATA_IDENTIFIER = "data";
-		String CORE_CONTEXT = "http://localhost:10101/ngsi-ld-core-context.json";
-		String LD_CONTEXT = "http://localhost:10101/water-quality-observed-context.json";
+		final String DATA_IDENTIFIER = "data";
+		final String CORE_CONTEXT = "http://localhost:10101/ngsi-ld-core-context.json";
+		final String LD_CONTEXT = "http://localhost:10101/water-quality-observed-context.json";
 
-		String ngsiV2Content = Files.readString(Path.of("src/test/resources/ngsiV2_wqo_input.json"));
+		final String ngsiV2Content = Files.readString(Path.of("src/test/resources/ngsiV2_wqo_input.json"));
 
 		testRunner.setProperty("DATA_IDENTIFIER", DATA_IDENTIFIER);
 		testRunner.setProperty("CORE_CONTEXT", CORE_CONTEXT);
@@ -33,5 +35,24 @@ class NgsiV2ToLdProcessorTest {
 
 		testRunner.assertQueueEmpty();
 		testRunner.assertTransferCount(FlowManager.SUCCESS, 1);
+	}
+
+	@Test
+	void testFailFlow() throws Exception {
+		final String DATA_IDENTIFIER = "data";
+		final String CORE_CONTEXT = "http://localhost:10101/ngsi-ld-core-context.json";
+		final String LD_CONTEXT = "http://localhost:10101/water-quality-observed-context.json";
+
+		final String ngsiV2Content = Files.readString(Path.of("src/test/resources/ngsiV2_wqo_input.json"));
+
+		testRunner.setProperty("DATA_IDENTIFIER", DATA_IDENTIFIER);
+		testRunner.setProperty("CORE_CONTEXT", CORE_CONTEXT);
+		testRunner.setProperty("LD_CONTEXT", LD_CONTEXT);
+
+		testRunner.enqueue(ngsiV2Content, Map.of("mime.type", "wrong type"));
+		testRunner.run();
+
+		testRunner.assertQueueEmpty();
+		testRunner.assertTransferCount(FAILURE, 1);
 	}
 }
