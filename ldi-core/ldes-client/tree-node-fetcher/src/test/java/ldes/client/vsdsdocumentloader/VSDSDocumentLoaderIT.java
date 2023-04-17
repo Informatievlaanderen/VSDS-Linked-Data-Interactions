b847a55b64ee78f4c08b2b3805594127ec07d4b8
Stijn Verholen <boro@inuits.eu>
2023-04-17 16:29:29 +0200

    fix: issue #131 Speed of RDFJRepositoryMaterialisationProcessorTest: remove wrong-branch-commit

--- ldi-core/ldes-client/tree-node-fetcher/src/test/java/ldes/client/vsdsdocumentloader/VSDSDocumentLoaderIT.java
+++
@@ -1,8 +0,0 @@
-package ldes.client.vsdsdocumentloader;
-
-//@Suite
-//@IncludeEngines("cucumber")
-//@SelectClasspathResource("features/vsds-document-loader")
-//@ConfigurationParameter(key = "ldes.client.vsdsdocumentloader", value = "ldes.client.vsdsdocumentloader")
-public class VSDSDocumentLoaderIT {
-}
