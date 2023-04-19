12566d512a29fae5216801ea5833f23ec1838625
Jan Robert <jan.robert@cegeka.com>
2023-04-19 17:00:47 +0200

    feat: Ldio v2 UI

---
+++ ldi-orchestrator/ldio-ui/src/views/AboutView.vue
@@ -0,0 +1,31 @@
+<!-- Flowchart.vue -->
+<script setup>
+import {VueFlow} from '@vue-flow/core'
+import {Background} from '@vue-flow/background'
+
+import {ref} from "vue";
+
+const elements = ref([
+  {
+    id: '1',
+    label: 'node 1',
+    position: { x: 100, y: 100 },
+  },
+  {
+    id: '2',
+    label: 'node 2',
+    position: { x: 100, y: 200 },
+  },
+  {
+    id: 'e1-2',
+    target: '2',
+    source: '1',
+  },
+])
+</script>
+
+<template>
+  <VueFlow v-model="elements" fit-view-on-init class="vue-flow-basic-example" style="height: 39em">
+    <Background />
+  </VueFlow>
+</template>
