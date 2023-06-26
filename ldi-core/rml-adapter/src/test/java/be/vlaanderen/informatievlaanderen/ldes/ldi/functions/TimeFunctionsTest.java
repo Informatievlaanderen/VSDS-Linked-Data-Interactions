4370885dd7ddf2437c0eae0c14d18326b7e8cf17
Jan Robert <jan.robert@cegeka.com>
2023-06-26 16:30:45 +0200

    sonar

--- ldi-core/rml-adapter/src/test/java/be/vlaanderen/informatievlaanderen/ldes/ldi/functions/TimeFunctionsTest.java
+++ ldi-core/rml-adapter/src/test/java/be/vlaanderen/informatievlaanderen/ldes/ldi/functions/TimeFunctionsTest.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-public class TimeFunctionsTest {
+class TimeFunctionsTest {
 	TimeFunctions timeFunctions = new TimeFunctions();
 
 	@Test
