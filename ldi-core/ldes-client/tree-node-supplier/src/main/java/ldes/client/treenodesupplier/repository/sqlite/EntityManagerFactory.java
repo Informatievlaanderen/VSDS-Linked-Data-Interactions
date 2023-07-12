03deb44852500d1c5e62293805027215eb77b2e4
Wouter Lefever <wouter.lefever@cegeka.com>
2023-07-12 11:00:10 +0200

    Feature: sqlite db in separate directory (#253)

--- ldi-core/ldes-client/tree-node-supplier/src/main/java/ldes/client/treenodesupplier/repository/sqlite/EntityManagerFactory.java
+++ ldi-core/ldes-client/tree-node-supplier/src/main/java/ldes/client/treenodesupplier/repository/sqlite/EntityManagerFactory.java
@@ -1,10 +1,12 @@
 package ldes.client.treenodesupplier.repository.sqlite;
 
+import ldes.client.treenodesupplier.repository.exception.CreateDirectoryFailedException;
 import ldes.client.treenodesupplier.repository.exception.DestroyDbFailedException;
 
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.Paths;
 
 import javax.persistence.EntityManager;
 import javax.persistence.Persistence;
@@ -12,18 +14,26 @@ import javax.persistence.Persistence;
 @SuppressWarnings("java:S2696")
 public class EntityManagerFactory {
 
+	public static final String DATABASE_DIRECTORY = "state";
+	public static final String PERSISTENCE_UNIT_NAME = "pu-sqlite-jpa";
+	public static final String DATABASE_NAME = "database.db";
 	private static EntityManagerFactory instance = null;
 	private final EntityManager em;
 	private final javax.persistence.EntityManagerFactory emf;
 	private boolean databaseDeleted = false;
 
 	private EntityManagerFactory() {
-		emf = Persistence.createEntityManagerFactory("pu-sqlite-jpa");
+		emf = Persistence.createEntityManagerFactory(PERSISTENCE_UNIT_NAME);
 		em = emf.createEntityManager();
 	}
 
 	public static synchronized EntityManagerFactory getInstance() {
 		if (instance == null) {
+			try {
+				Files.createDirectory(Paths.get(DATABASE_DIRECTORY));
+			} catch (IOException e) {
+				throw new CreateDirectoryFailedException(e);
+			}
 			instance = new EntityManagerFactory();
 		}
 
@@ -40,7 +50,8 @@ public class EntityManagerFactory {
 			emf.close();
 			instance = null;
 			if (!databaseDeleted) {
-				Files.delete(Path.of("database.db"));
+				Files.delete(Path.of(DATABASE_DIRECTORY, DATABASE_NAME));
+				Files.delete(Path.of(DATABASE_DIRECTORY));
 				databaseDeleted = true;
 			}
 		} catch (IOException e) {
