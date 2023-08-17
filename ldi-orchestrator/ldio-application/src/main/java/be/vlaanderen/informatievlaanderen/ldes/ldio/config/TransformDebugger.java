0368849a35721ac87dfc868fb876a6fd2bff0839
Jan Robert <15772440+Yalz@users.noreply.github.com>
2023-08-17 10:38:22 +0200

    Feat: LDIO component debug logger (#287)

---
+++ ldi-orchestrator/ldio-application/src/main/java/be/vlaanderen/informatievlaanderen/ldes/ldio/config/TransformDebugger.java
@@ -0,0 +1,24 @@
+package be.vlaanderen.informatievlaanderen.ldes.ldio.config;
+
+import be.vlaanderen.informatievlaanderen.ldes.ldi.types.LdiTransformer;
+import org.apache.jena.rdf.model.Model;
+import org.apache.jena.riot.Lang;
+import org.apache.jena.riot.RDFWriter;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class TransformDebugger implements LdiTransformer {
+	private final Logger log;
+	private final LdiTransformer ldiTransformer;
+
+	public TransformDebugger(LdiTransformer ldiTransformer) {
+		this.ldiTransformer = ldiTransformer;
+		log = LoggerFactory.getLogger(ldiTransformer.getClass());
+	}
+
+	@Override
+	public Model apply(Model model) {
+		log.debug("Starting model: \n" + RDFWriter.source(model).lang(Lang.TTL).asString());
+		return ldiTransformer.apply(model);
+	}
+}
