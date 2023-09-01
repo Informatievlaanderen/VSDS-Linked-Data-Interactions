5bd5041b6741376bd73918b4a5c7a0aa7636da50
Tom Van Besien <tom.vanbesien@gmail.com>
2023-09-01 16:11:42 +0200

    format

--- ldi-core/model-splitter/src/main/java/be/vlaanderen/informatievlaanderen/ldes/ldi/ModelSplitAdapter.java
+++ ldi-core/model-splitter/src/main/java/be/vlaanderen/informatievlaanderen/ldes/ldi/ModelSplitAdapter.java
@@ -8,18 +8,18 @@ import java.util.stream.Stream;
 // TODO TVB: 01/09/23 test me
 public class ModelSplitAdapter implements LdiAdapter {
 
-    private final String subjectType;
-    private final LdiAdapter ldiAdapter;
-    private final ModelSplitter modelSplitter = new ModelSplitter();
+	private final String subjectType;
+	private final LdiAdapter ldiAdapter;
+	private final ModelSplitter modelSplitter = new ModelSplitter();
 
-    public ModelSplitAdapter(String subjectType, LdiAdapter ldiAdapter) {
-        this.subjectType = subjectType;
-        this.ldiAdapter = ldiAdapter;
-    }
+	public ModelSplitAdapter(String subjectType, LdiAdapter ldiAdapter) {
+		this.subjectType = subjectType;
+		this.ldiAdapter = ldiAdapter;
+	}
 
-    @Override
-    public Stream<Model> apply(Content content) {
-        return ldiAdapter.apply(content).flatMap(model -> modelSplitter.split(model, subjectType).stream());
-    }
+	@Override
+	public Stream<Model> apply(Content content) {
+		return ldiAdapter.apply(content).flatMap(model -> modelSplitter.split(model, subjectType).stream());
+	}
 
 }
