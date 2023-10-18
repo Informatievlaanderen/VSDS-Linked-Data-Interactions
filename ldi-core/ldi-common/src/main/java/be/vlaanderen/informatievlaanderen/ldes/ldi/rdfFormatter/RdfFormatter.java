35732a9da03949ec2a3f87950ea9584cf55d88cc
Yalz <Yalz@users.noreply.github.com>
2023-10-18 18:14:12 +0200

    feat: RDF Formatter for JSON-LD framing: WIP

---
+++ ldi-core/ldi-common/src/main/java/be/vlaanderen/informatievlaanderen/ldes/ldi/rdfFormatter/RdfFormatter.java
@@ -0,0 +1,53 @@
+package be.vlaanderen.informatievlaanderen.ldes.ldi.rdfFormatter;
+
+import com.github.jsonldjava.core.JsonLdOptions;
+import org.apache.commons.cli.MissingArgumentException;
+import org.apache.jena.atlas.json.JsonObject;
+import org.apache.jena.atlas.json.io.parser.JSONParser;
+import org.apache.jena.rdf.model.Model;
+import org.apache.jena.riot.JsonLDWriteContext;
+import org.apache.jena.riot.Lang;
+import org.apache.jena.riot.RDFFormat;
+import org.apache.jena.riot.RDFWriter;
+import org.apache.jena.riot.writer.JsonLD10Writer;
+import org.apache.jena.sparql.util.Context;
+
+import static be.vlaanderen.informatievlaanderen.ldes.ldi.rdfFormatter.PrefixAdder.*;
+
+public class RdfFormatter {
+    public static String formatModel(Model model, Lang lang, String frameType) throws MissingArgumentException {
+
+        if (lang == Lang.JSONLD) {
+            if (frameType == null) {
+                throw new MissingArgumentException("No frameType was given");
+            }
+            return RDFWriter.source(addPrefixesToModel(model))
+                    .context(getFramedContext(model, frameType))
+                    .format(RDFFormat.JSONLD10_FRAME_PRETTY)
+                    .asString();
+        }
+        else {
+            return RDFWriter.source(model)
+                    .lang(lang)
+                    .asString();
+        }
+    }
+
+    protected static Context getFramedContext(Model model, String frameType) {
+        JsonLDWriteContext jenaCtx = new JsonLDWriteContext();
+
+        JsonObject context = new JsonObject();
+        model.getGraph().getPrefixMapping().getNsPrefixMap().forEach(context::put);
+
+        JsonObject frame = new JsonObject();
+        frame.put("@context", context);
+        frame.put("@type", frameType);
+        jenaCtx.set(JsonLD10Writer.JSONLD_FRAME, frame.toString());
+
+        JsonLdOptions jsonLdOptions = new JsonLdOptions();
+        jsonLdOptions.setOmitGraph(true);
+        jenaCtx.setOptions(jsonLdOptions);
+
+        return jenaCtx;
+    }
+}
---
+++ ldi-core/ldi-common/src/test/java/be/vlaanderen/informatievlaanderen/ldes/ldi/rdfFormatter/PrefixAdderTest.java
@@ -0,0 +1,43 @@
+package be.vlaanderen.informatievlaanderen.ldes.ldi.rdfFormatter;
+
+import org.apache.jena.rdf.model.Model;
+import org.apache.jena.riot.Lang;
+import org.apache.jena.riot.RDFParserBuilder;
+import org.junit.jupiter.api.Test;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Map;
+import java.util.Objects;
+import java.util.stream.Collectors;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PrefixAdderTest {
+
+	@Test
+	void when_PrefixesAndLocalNamesAreValid_TheyAreAddedToPrefixMap() throws URISyntaxException, IOException {
+		Model model = readLdesMemberFromFile(getClass().getClassLoader(), "rdfFormatter/prefixAdderTest.nq");
+		Model updatedModel = PrefixAdder.addPrefixesToModel(model);
+		Map<String, String> nsPrefixMap = updatedModel.getNsPrefixMap();
+
+		// verify that there is no prefix for a namespace if ONE or MORE of its
+		// localNames is invalid.
+		assertFalse(nsPrefixMap.containsKey("bad"));
+
+		assertTrue(nsPrefixMap.containsKey("ical"));
+		assertEquals(1, nsPrefixMap.size());
+	}
+
+	private Model readLdesMemberFromFile(ClassLoader classLoader, String fileName)
+			throws URISyntaxException, IOException {
+		File file = new File(Objects.requireNonNull(classLoader.getResource(fileName)).toURI());
+
+		return RDFParserBuilder.create()
+				.fromString(Files.lines(Paths.get(file.toURI())).collect(Collectors.joining())).lang(Lang.NQUADS)
+				.toModel();
+	}
+}
