081a4374d7498a944d7ae458fd19c5ce12a311f8
larsvke <lars.vankerckhoven@cegeka.com>
2023-10-23 12:55:15 +0200

    feat: geo-sparql-functions-sonarissuesFix:
    use lowercase for variables
    spearated declarations

--- ldi-core/sparql-construct/src/main/java/be/vlaanderen/informatievlaanderen/ldes/ldi/utils/SparqlFunctionsUtils.java
+++ ldi-core/sparql-construct/src/main/java/be/vlaanderen/informatievlaanderen/ldes/ldi/utils/SparqlFunctionsUtils.java
@@ -25,20 +25,25 @@ public class SparqlFunctionsUtils {
 	 */
 	public static double calculateDistance(double latitude1, double longitude1, double latitude2, double longitude2) {
 
-		double U1 = Math.atan((1 - FLATTENING) * Math.tan(Math.toRadians(latitude1)));
-		double U2 = Math.atan((1 - FLATTENING) * Math.tan(Math.toRadians(latitude2)));
+		double u1 = Math.atan((1 - FLATTENING) * Math.tan(Math.toRadians(latitude1)));
+		double u2 = Math.atan((1 - FLATTENING) * Math.tan(Math.toRadians(latitude2)));
 
-		double sinU1 = Math.sin(U1);
-		double cosU1 = Math.cos(U1);
-		double sinU2 = Math.sin(U2);
-		double cosU2 = Math.cos(U2);
+		double sinU1 = Math.sin(u1);
+		double cosU1 = Math.cos(u1);
+		double sinU2 = Math.sin(u2);
+		double cosU2 = Math.cos(u2);
 
 		double longitudeDifference = Math.toRadians(longitude2 - longitude1);
 		double previousLongitudeDifference;
 
-		double sinSigma, cosSigma, sigma, sinAlpha, cosSqAlpha, cos2SigmaM;
+		double sinSigma;
+        double cosSigma;
+        double sigma;
+        double sinAlpha;
+        double cosSqAlpha;
+        double cos2SigmaM;
 
-		do {
+        do {
 			sinSigma = Math.sqrt(Math.pow(cosU2 * Math.sin(longitudeDifference), 2) +
 					Math.pow(cosU1 * sinU2 - sinU1 * cosU2 * Math.cos(longitudeDifference), 2));
 			cosSigma = sinU1 * sinU2 + cosU1 * cosU2 * Math.cos(longitudeDifference);
@@ -50,21 +55,21 @@ public class SparqlFunctionsUtils {
 				cos2SigmaM = 0;
 			}
 			previousLongitudeDifference = longitudeDifference;
-			double C = FLATTENING / 16 * cosSqAlpha * (4 + FLATTENING * (4 - 3 * cosSqAlpha));
-			longitudeDifference = Math.toRadians(longitude2 - longitude1) + (1 - C) * FLATTENING * sinAlpha *
-					(sigma + C * sinSigma * (cos2SigmaM + C * cosSigma * (-1 + 2 * Math.pow(cos2SigmaM, 2))));
+			double c = FLATTENING / 16 * cosSqAlpha * (4 + FLATTENING * (4 - 3 * cosSqAlpha));
+			longitudeDifference = Math.toRadians(longitude2 - longitude1) + (1 - c) * FLATTENING * sinAlpha *
+					(sigma + c * sinSigma * (cos2SigmaM + c * cosSigma * (-1 + 2 * Math.pow(cos2SigmaM, 2))));
 		} while (Math.abs(longitudeDifference - previousLongitudeDifference) > ERROR_TOLERANCE);
 
 		double uSq = cosSqAlpha * (Math.pow(SEMI_MAJOR_AXIS_MT, 2) - Math.pow(SEMI_MINOR_AXIS_MT, 2))
 				/ Math.pow(SEMI_MINOR_AXIS_MT, 2);
 
-		double A = 1 + uSq / 16384 * (4096 + uSq * (-768 + uSq * (320 - 175 * uSq)));
-		double B = uSq / 1024 * (256 + uSq * (-128 + uSq * (74 - 47 * uSq)));
+		double a = 1 + uSq / 16384 * (4096 + uSq * (-768 + uSq * (320 - 175 * uSq)));
+		double b = uSq / 1024 * (256 + uSq * (-128 + uSq * (74 - 47 * uSq)));
 
-		double deltaSigma = B * sinSigma * (cos2SigmaM + B / 4 * (cosSigma * (-1 + 2 * Math.pow(cos2SigmaM, 2))
-				- B / 6 * cos2SigmaM * (-3 + 4 * Math.pow(sinSigma, 2)) * (-3 + 4 * Math.pow(cos2SigmaM, 2))));
+		double deltaSigma = b * sinSigma * (cos2SigmaM + b / 4 * (cosSigma * (-1 + 2 * Math.pow(cos2SigmaM, 2))
+				- b / 6 * cos2SigmaM * (-3 + 4 * Math.pow(sinSigma, 2)) * (-3 + 4 * Math.pow(cos2SigmaM, 2))));
 
-		return SEMI_MINOR_AXIS_MT * A * (sigma - deltaSigma);
+		return SEMI_MINOR_AXIS_MT * a * (sigma - deltaSigma);
 	}
 
 	public static double[] getLineLengths(Coordinate[] coords) {
