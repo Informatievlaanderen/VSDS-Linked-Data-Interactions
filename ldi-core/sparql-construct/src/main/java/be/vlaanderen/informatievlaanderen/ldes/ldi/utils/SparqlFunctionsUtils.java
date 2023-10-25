61398f6b365a867ca36535536a6c9974a5a730ed
larsic <lars.vankerckhoven@cegeka.com>
2023-10-25 09:14:36 +0200

    Feat/geo sparql functions sonar issues fix final (#378)
    Signed-off-by: dependabot[bot] <support@github.com>
    Co-authored-by: Tomvbe <34196062+Tomvbe@users.noreply.github.com>
    Co-authored-by: Jan Robert <jan.robert@cegeka.com>
    Co-authored-by: Samuel Van Ackere <samuel.vanackere@vlaanderen.be>
    Co-authored-by: Jan Robert <15772440+Yalz@users.noreply.github.com>
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>
    Co-authored-by: Yalz <Yalz@users.noreply.github.com>

---
+++ ldi-core/sparql-construct/src/main/java/be/vlaanderen/informatievlaanderen/ldes/ldi/utils/SparqlFunctionsUtils.java
@@ -0,0 +1,111 @@
+package be.vlaanderen.informatievlaanderen.ldes.ldi.utils;
+
+import org.apache.jena.geosparql.implementation.GeometryWrapper;
+import org.apache.jena.geosparql.implementation.GeometryWrapperFactory;
+import org.apache.jena.graph.Node;
+import org.apache.jena.sparql.expr.NodeValue;
+import org.locationtech.jts.geom.Coordinate;
+
+import java.util.stream.DoubleStream;
+import java.util.stream.IntStream;
+
+public class SparqlFunctionsUtils {
+
+	static final double SEMI_MAJOR_AXIS_MT = 6378137;
+	static final double SEMI_MINOR_AXIS_MT = 6356752.314245;
+	static final double FLATTENING = 1 / 298.257223563;
+	static final double ERROR_TOLERANCE = 1e-12;
+
+	private SparqlFunctionsUtils() {
+	}
+
+	/**
+	 * Vincenty's Formula
+	 * returns a double representing the distance in meters
+	 */
+	public static double calculateDistance(double latitude1, double longitude1, double latitude2, double longitude2) {
+
+		double u1 = Math.atan((1 - FLATTENING) * Math.tan(Math.toRadians(latitude1)));
+		double u2 = Math.atan((1 - FLATTENING) * Math.tan(Math.toRadians(latitude2)));
+
+		double sinU1 = Math.sin(u1);
+		double cosU1 = Math.cos(u1);
+		double sinU2 = Math.sin(u2);
+		double cosU2 = Math.cos(u2);
+
+		double longitudeDifference = Math.toRadians(longitude2 - longitude1);
+		double previousLongitudeDifference;
+
+		double sinSigma;
+		double cosSigma;
+		double sigma;
+		double sinAlpha;
+		double cosSqAlpha;
+		double cos2SigmaM;
+
+		do {
+			sinSigma = Math.sqrt(Math.pow(cosU2 * Math.sin(longitudeDifference), 2) +
+					Math.pow(cosU1 * sinU2 - sinU1 * cosU2 * Math.cos(longitudeDifference), 2));
+			cosSigma = sinU1 * sinU2 + cosU1 * cosU2 * Math.cos(longitudeDifference);
+			sigma = Math.atan2(sinSigma, cosSigma);
+			sinAlpha = cosU1 * cosU2 * Math.sin(longitudeDifference) / sinSigma;
+			cosSqAlpha = 1 - Math.pow(sinAlpha, 2);
+			cos2SigmaM = cosSigma - 2 * sinU1 * sinU2 / cosSqAlpha;
+			if (Double.isNaN(cos2SigmaM)) {
+				cos2SigmaM = 0;
+			}
+			previousLongitudeDifference = longitudeDifference;
+			double c = FLATTENING / 16 * cosSqAlpha * (4 + FLATTENING * (4 - 3 * cosSqAlpha));
+			longitudeDifference = Math.toRadians(longitude2 - longitude1) + (1 - c) * FLATTENING * sinAlpha *
+					(sigma + c * sinSigma * (cos2SigmaM + c * cosSigma * (-1 + 2 * Math.pow(cos2SigmaM, 2))));
+		} while (Math.abs(longitudeDifference - previousLongitudeDifference) > ERROR_TOLERANCE);
+
+		double uSq = cosSqAlpha * (Math.pow(SEMI_MAJOR_AXIS_MT, 2) - Math.pow(SEMI_MINOR_AXIS_MT, 2))
+				/ Math.pow(SEMI_MINOR_AXIS_MT, 2);
+
+		double a = 1 + uSq / 16384 * (4096 + uSq * (-768 + uSq * (320 - 175 * uSq)));
+		double b = uSq / 1024 * (256 + uSq * (-128 + uSq * (74 - 47 * uSq)));
+
+		double deltaSigma = b * sinSigma * (cos2SigmaM + b / 4 * (cosSigma * (-1 + 2 * Math.pow(cos2SigmaM, 2))
+				- b / 6 * cos2SigmaM * (-3 + 4 * Math.pow(sinSigma, 2)) * (-3 + 4 * Math.pow(cos2SigmaM, 2))));
+
+		return SEMI_MINOR_AXIS_MT * a * (sigma - deltaSigma);
+	}
+
+	public static double[] getLineLengths(Coordinate[] coords) {
+
+		return getLineLengthsStream(coords)
+				.toArray();
+	}
+
+	public static double getTotalLineLength(Coordinate[] coords) {
+
+		return getLineLengthsStream(coords)
+				.sum();
+	}
+
+	private static DoubleStream getLineLengthsStream(Coordinate[] coords) {
+
+		return IntStream.range(0, coords.length - 1)
+				.mapToDouble(i -> calculateDistance(coords[i].y, coords[i].x, coords[i + 1].y, coords[i + 1].x));
+	}
+
+	public static Coordinate findOnSegmentByDistance(Coordinate c1, Coordinate c2, double distanceToX) {
+
+		double length = calculateDistance(c1.y, c1.x, c2.y, c2.x);
+		double ratio = distanceToX / length;
+		double x = ratio * c2.x + (1.0 - ratio) * c1.x;
+		double y = ratio * c2.y + (1.0 - ratio) * c1.y;
+
+		return new Coordinate(x, y);
+	}
+
+	public static NodeValue getNodeValue(GeometryWrapper wrapper, Coordinate result) {
+
+		Node midpoint = GeometryWrapperFactory
+				.createPoint(result, wrapper.getGeometryDatatypeURI())
+				.asNode();
+
+		return NodeValue.makeNode(midpoint);
+	}
+}
