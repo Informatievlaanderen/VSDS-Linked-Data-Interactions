da8386cd3c27c3bcdc09ca6882d4fd9c352fdec0
Yalz <Yalz@users.noreply.github.com>
2023-10-27 17:11:49 +0200

    fix: RDF-Writer: formatter
    (cherry picked from commit 976998408b621b1f46ecbe18b3adafaf679a660e)

--- ldi-core/ldi-common/src/test/java/be/vlaanderen/informatievlaanderen/ldes/ldi/rdf/formatter/LdiRdfWriterPropertiesTest.java
+++ ldi-core/ldi-common/src/test/java/be/vlaanderen/informatievlaanderen/ldes/ldi/rdf/formatter/LdiRdfWriterPropertiesTest.java
@@ -10,32 +10,32 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class LdiRdfWriterPropertiesTest {
 
-    private final String FRAME_TYPE_PERSON = "http://schema.org/Person";
+	private final String FRAME_TYPE_PERSON = "http://schema.org/Person";
 
-    private final Map<String, String> ldioStrippedProperties = Map.of(stripped(CONTENT_TYPE), "application/ld+json",
-            stripped(FRAME_TYPE), FRAME_TYPE_PERSON);
+	private final Map<String, String> ldioStrippedProperties = Map.of(stripped(CONTENT_TYPE), "application/ld+json",
+			stripped(FRAME_TYPE), FRAME_TYPE_PERSON);
 
-    private final Map<String, String> properties = Map.of(stripped(CONTENT_TYPE), "application/ld+json",
-            stripped(FRAME_TYPE), FRAME_TYPE_PERSON);
+	private final Map<String, String> properties = Map.of(stripped(CONTENT_TYPE), "application/ld+json",
+			stripped(FRAME_TYPE), FRAME_TYPE_PERSON);
 
-    @Test
-    void validateGetLang() {
-        assertEquals(Lang.JSONLD, new LdiRdfWriterProperties(properties).getLang());
-    }
+	@Test
+	void validateGetLang() {
+		assertEquals(Lang.JSONLD, new LdiRdfWriterProperties(properties).getLang());
+	}
 
-    @Test
-    void validateLdioGetLang() {
-        assertEquals(Lang.JSONLD, new LdiRdfWriterProperties(ldioStrippedProperties).getLang());
-    }
+	@Test
+	void validateLdioGetLang() {
+		assertEquals(Lang.JSONLD, new LdiRdfWriterProperties(ldioStrippedProperties).getLang());
+	}
 
-    @Test
-    void validateGetFrame() {
-        assertEquals(FRAME_TYPE_PERSON, new LdiRdfWriterProperties(properties).getFrameType());
-    }
+	@Test
+	void validateGetFrame() {
+		assertEquals(FRAME_TYPE_PERSON, new LdiRdfWriterProperties(properties).getFrameType());
+	}
 
-    @Test
-    void validateLdioGetFrame() {
-        assertEquals(FRAME_TYPE_PERSON, new LdiRdfWriterProperties(ldioStrippedProperties).getFrameType());
-    }
+	@Test
+	void validateLdioGetFrame() {
+		assertEquals(FRAME_TYPE_PERSON, new LdiRdfWriterProperties(ldioStrippedProperties).getFrameType());
+	}
 
 }
