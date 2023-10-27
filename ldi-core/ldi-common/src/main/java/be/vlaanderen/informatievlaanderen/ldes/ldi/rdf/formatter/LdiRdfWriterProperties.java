e936effe38f49300ebc51cd5bca47000a1174daf
Yalz <Yalz@users.noreply.github.com>
2023-10-27 16:34:30 +0200

    fix: RDF-Writer: invalid key for frame-type
    (cherry picked from commit a70644b5c62aaddf2f2e607c46ae8a7ee4fefafc)

--- ldi-core/ldi-common/src/main/java/be/vlaanderen/informatievlaanderen/ldes/ldi/rdf/formatter/LdiRdfWriterProperties.java
+++ ldi-core/ldi-common/src/main/java/be/vlaanderen/informatievlaanderen/ldes/ldi/rdf/formatter/LdiRdfWriterProperties.java
@@ -4,14 +4,13 @@ import org.apache.jena.atlas.web.MediaType;
 import org.apache.jena.riot.Lang;
 
 import java.util.Map;
-import java.util.Objects;
 
 import static org.apache.jena.riot.RDFLanguages.nameToLang;
 
 public class LdiRdfWriterProperties {
-	public final static String RDF_WRITER = "rdfwriter";
-	public final static String CONTENT_TYPE = "contenttype";
-	public final static String FRAME_TYPE = "jsonldframetype";
+	public final static String RDF_WRITER = "rdf-writer";
+	public final static String CONTENT_TYPE = "content-type";
+	public final static String FRAME_TYPE = "frame-type";
 	private final Map<String, String> properties;
 	private Lang lang;
 
@@ -34,8 +33,8 @@ public class LdiRdfWriterProperties {
 	}
 
 	private void setLangFromProperties() {
-		if (properties.containsKey(CONTENT_TYPE)) {
-			this.lang = nameToLang(MediaType.createFromContentType(properties.get(CONTENT_TYPE)).getContentTypeStr());
+		if (properties.containsKey(stripped(CONTENT_TYPE))) {
+			this.lang = nameToLang(MediaType.createFromContentType(getProperty(CONTENT_TYPE)).getContentTypeStr());
 		} else {
 			this.lang = Lang.TTL;
 		}
@@ -44,6 +43,19 @@ public class LdiRdfWriterProperties {
 	// JSON-LD
 
 	public String getFrameType() {
-		return properties.get(Objects.requireNonNull(FRAME_TYPE));
+		return getProperty(FRAME_TYPE);
+	}
+
+	// Common
+
+	private String getProperty(String key) {
+		return properties.get(stripped(key));
+	}
+
+	static String stripped(String key) {
+		return key
+				.toLowerCase()
+				.replace("-", "")
+				.replace("_", "");
 	}
 }
---
+++ ldi-core/ldi-common/src/test/java/be/vlaanderen/informatievlaanderen/ldes/ldi/rdf/formatter/LdiRdfWriterPropertiesTest.java
@@ -0,0 +1,41 @@
+package be.vlaanderen.informatievlaanderen.ldes.ldi.rdf.formatter;
+
+import org.apache.jena.riot.Lang;
+import org.junit.jupiter.api.Test;
+
+import java.util.Map;
+
+import static be.vlaanderen.informatievlaanderen.ldes.ldi.rdf.formatter.LdiRdfWriterProperties.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+class LdiRdfWriterPropertiesTest {
+
+    private final String FRAME_TYPE_PERSON = "http://schema.org/Person";
+
+    private final Map<String, String> ldioStrippedProperties = Map.of(stripped(CONTENT_TYPE), "application/ld+json",
+            stripped(FRAME_TYPE), FRAME_TYPE_PERSON);
+
+    private final Map<String, String> properties = Map.of(stripped(CONTENT_TYPE), "application/ld+json",
+            stripped(FRAME_TYPE), FRAME_TYPE_PERSON);
+
+    @Test
+    void validateGetLang() {
+        assertEquals(Lang.JSONLD, new LdiRdfWriterProperties(properties).getLang());
+    }
+
+    @Test
+    void validateLdioGetLang() {
+        assertEquals(Lang.JSONLD, new LdiRdfWriterProperties(ldioStrippedProperties).getLang());
+    }
+
+    @Test
+    void validateGetFrame() {
+        assertEquals(FRAME_TYPE_PERSON, new LdiRdfWriterProperties(properties).getFrameType());
+    }
+
+    @Test
+    void validateLdioGetFrame() {
+        assertEquals(FRAME_TYPE_PERSON, new LdiRdfWriterProperties(ldioStrippedProperties).getFrameType());
+    }
+
+}
