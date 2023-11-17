5b75dc2d8a1f3d37bf34b8a75659cd9b05b5b173
Tom Van Besien <tom.vanbesien@gmail.com>
2023-11-17 14:22:10 +0100

    Test withs

--- ldi-core/request-executor/src/test/java/be/vlaanderen/informatievlaanderen/ldes/ldi/requestexecutor/valueobjects/PostRequestTest.java
+++ ldi-core/request-executor/src/test/java/be/vlaanderen/informatievlaanderen/ldes/ldi/requestexecutor/valueobjects/PostRequestTest.java
@@ -97,4 +97,24 @@ class PostRequestTest {
 
 	}
 
+	@Test
+	void test_WithUrl() {
+		var initialRequest = new PostRequest("url", new RequestHeaders(List.of(new RequestHeader("key", "val"))), "body");
+
+		var requestWithOtherUrl = initialRequest.with("other-url");
+		assertEquals(initialRequest.getRequestHeaders(), requestWithOtherUrl.getRequestHeaders());
+		assertEquals(initialRequest.getMethod(), requestWithOtherUrl.getMethod());
+		assertNotEquals(initialRequest.getUrl(), requestWithOtherUrl.getUrl());
+	}
+
+	@Test
+	void test_WithRequestHeaders() {
+		var initialRequest = new PostRequest("url", new RequestHeaders(List.of(new RequestHeader("key", "val"))), "body");
+
+		var requestWithOtherUrl = initialRequest.with(new RequestHeaders(List.of(new RequestHeader("other-key", "val"))));
+		assertNotEquals(initialRequest.getRequestHeaders(), requestWithOtherUrl.getRequestHeaders());
+		assertEquals(initialRequest.getMethod(), requestWithOtherUrl.getMethod());
+		assertEquals(initialRequest.getUrl(), requestWithOtherUrl.getUrl());
+	}
+
 }
