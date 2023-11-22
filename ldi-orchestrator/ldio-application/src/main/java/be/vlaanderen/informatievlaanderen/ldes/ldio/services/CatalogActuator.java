539c4be53cc12d814d6645b47b19d6ec56cfdd33
Yalz <Yalz@users.noreply.github.com>
2023-11-22 14:54:53 +0100

    docs

--- ldi-orchestrator/ldio-application/src/main/java/be/vlaanderen/informatievlaanderen/ldes/ldio/services/CatalogActuator.java
+++ ldi-orchestrator/ldio-application/src/main/java/be/vlaanderen/informatievlaanderen/ldes/ldio/services/CatalogActuator.java
@@ -4,50 +4,43 @@ import be.vlaanderen.informatievlaanderen.ldes.ldio.configurator.LdioAdapterConf
 import be.vlaanderen.informatievlaanderen.ldes.ldio.configurator.LdioInputConfigurator;
 import be.vlaanderen.informatievlaanderen.ldes.ldio.configurator.LdioOutputConfigurator;
 import be.vlaanderen.informatievlaanderen.ldes.ldio.configurator.LdioTransformerConfigurator;
+import org.springframework.boot.actuate.endpoint.annotation.Endpoint;
+import org.springframework.boot.actuate.endpoint.annotation.ReadOperation;
 import org.springframework.context.ConfigurableApplicationContext;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Objects;
 import java.util.Set;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 @RestController
-@RequestMapping(path = "/admin/api/v1/catalog")
-public class CatalogController {
+@Endpoint(id = "catalog")
+public class CatalogActuator {
 	private final ConfigurableApplicationContext context;
 
-	public CatalogController(ConfigurableApplicationContext context) {
+	public CatalogActuator(ConfigurableApplicationContext context) {
 		this.context = context;
 	}
 
-	@GetMapping
-	public Set<String> catalog() {
-		return Stream.of(inputs(), adapters(), transformers(), outputs())
-				.filter(Objects::nonNull)
-				.flatMap(Set::stream)
-				.collect(Collectors.toSet());
+	@ReadOperation
+	public LdioCatalog catalog() {
+		return new LdioCatalog(inputs(), adapters(), transformers(), outputs());
 	}
 
-	@GetMapping(path = "/inputs")
-	public Set<String> inputs() {
+	private Set<String> inputs() {
 		return context.getBeansOfType(LdioInputConfigurator.class).keySet();
 	}
 
-	@GetMapping(path = "/adapters")
-	public Set<String> adapters() {
+	private Set<String> adapters() {
 		return context.getBeansOfType(LdioAdapterConfigurator.class).keySet();
 	}
 
-	@GetMapping(path = "/transformers")
-	public Set<String> transformers() {
+	private Set<String> transformers() {
 		return context.getBeansOfType(LdioTransformerConfigurator.class).keySet();
 	}
 
-	@GetMapping(path = "/outputs")
-	public Set<String> outputs() {
+	private Set<String> outputs() {
 		return context.getBeansOfType(LdioOutputConfigurator.class).keySet();
+	}
+
+	public record LdioCatalog(Set<String> inputs, Set<String> adapters, Set<String> transformers, Set<String> outputs) {
 	}
 }
