c8e60bcb49f5cca504e553f4f461878cd93eadda
Tomvbe <34196062+Tomvbe@users.noreply.github.com>
2024-01-02 10:36:11 +0100

    feat: remove member statement and reverse properties in ldesclient (#446)

--- ldi-core/ldes-client/tree-node-fetcher/src/main/java/ldes/client/treenodefetcher/domain/valueobjects/ModelResponse.java
+++ ldi-core/ldes-client/tree-node-fetcher/src/main/java/ldes/client/treenodefetcher/domain/valueobjects/ModelResponse.java
@@ -2,13 +2,7 @@ package ldes.client.treenodefetcher.domain.valueobjects;
 
 import ldes.client.treenodefetcher.domain.entities.TreeMember;
 import org.apache.jena.graph.TripleBoundary;
-import org.apache.jena.rdf.model.Model;
-import org.apache.jena.rdf.model.ModelExtract;
-import org.apache.jena.rdf.model.Property;
-import org.apache.jena.rdf.model.Resource;
-import org.apache.jena.rdf.model.Statement;
-import org.apache.jena.rdf.model.StatementTripleBoundary;
-import org.apache.jena.rdf.model.StmtIterator;
+import org.apache.jena.rdf.model.*;
 
 import java.util.Iterator;
 import java.util.List;
@@ -20,7 +14,6 @@ import static org.apache.jena.rdf.model.ResourceFactory.createProperty;
 public class ModelResponse {
 
 	protected static final Resource ANY_RESOURCE = null;
-	protected static final Property ANY_PROPERTY = null;
 	public static final String W3C_TREE = "https://w3id.org/tree#";
 	public static final Property W3ID_TREE_RELATION = createProperty(W3C_TREE, "relation");
 	public static final Property W3ID_TREE_MEMBER = createProperty(W3C_TREE, "member");
@@ -40,7 +33,8 @@ public class ModelResponse {
 	}
 
 	public List<TreeMember> getMembers() {
-		return extractMembers().map(memberStatement -> processMember(model, memberStatement))
+		return extractMembers()
+				.map(memberStatement -> processMember(model, memberStatement))
 				.toList();
 	}
 
@@ -52,22 +46,8 @@ public class ModelResponse {
 	}
 
 	private TreeMember processMember(Model treeNodeModel, Statement memberStatement) {
-		Model memberModel = modelExtract.extract(memberStatement.getObject().asResource(), treeNodeModel);
-		String id = memberStatement.getObject().toString();
-		memberModel.add(memberStatement);
-
-		treeNodeModel.listStatements(ANY_RESOURCE, ANY_PROPERTY, memberStatement.getResource())
-				.filterKeep(statement -> statement.getSubject().isURIResource()).filterDrop(memberStatement::equals)
-				.forEach(statement -> {
-					Model reversePropertyModel = modelExtract.extract(statement.getSubject(), treeNodeModel);
-					List<Statement> otherMembers = reversePropertyModel
-							.listStatements(statement.getSubject(), statement.getPredicate(), ANY_RESOURCE).toList();
-					otherMembers.forEach(otherMember -> reversePropertyModel
-							.remove(modelExtract.extract(otherMember.getResource(), reversePropertyModel)));
-
-					memberModel.add(reversePropertyModel);
-				});
-
+		final Model memberModel = modelExtract.extract(memberStatement.getObject().asResource(), treeNodeModel);
+		final String id = memberStatement.getObject().toString();
 		return new TreeMember(id, memberModel);
 	}
 
