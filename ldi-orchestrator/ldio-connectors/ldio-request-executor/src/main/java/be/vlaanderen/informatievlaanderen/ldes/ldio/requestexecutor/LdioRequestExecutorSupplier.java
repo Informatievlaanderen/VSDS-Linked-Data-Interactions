3efc85cf9d06b40dd59c2c9a4821211b41541198
jonasbul <jonas.bulcke@cegeka.com>
2024-01-23 14:15:15 +0100

    feat: customization of rate limiter

--- ldi-orchestrator/ldio-connectors/ldio-request-executor/src/main/java/be/vlaanderen/informatievlaanderen/ldes/ldio/requestexecutor/LdioRequestExecutorSupplier.java
+++ ldi-orchestrator/ldio-connectors/ldio-request-executor/src/main/java/be/vlaanderen/informatievlaanderen/ldes/ldio/requestexecutor/LdioRequestExecutorSupplier.java
@@ -27,90 +27,94 @@ import static org.apache.commons.lang3.ObjectUtils.isNotEmpty;
  */
 public class LdioRequestExecutorSupplier {
 
-	public static final String DEFAULT_API_KEY_HEADER = "X-API-KEY";
+    public static final String DEFAULT_API_KEY_HEADER = "X-API-KEY";
 
-	private final RequestExecutorFactory requestExecutorFactory;
+    private final RequestExecutorFactory requestExecutorFactory;
 
-	public LdioRequestExecutorSupplier() {
-		this(new RequestExecutorFactory());
-	}
+    public LdioRequestExecutorSupplier() {
+        this(new RequestExecutorFactory());
+    }
 
-	public LdioRequestExecutorSupplier(RequestExecutorFactory requestExecutorFactory) {
-		this.requestExecutorFactory = requestExecutorFactory;
-	}
+    public LdioRequestExecutorSupplier(RequestExecutorFactory requestExecutorFactory) {
+        this.requestExecutorFactory = requestExecutorFactory;
+    }
 
-	public RequestExecutor getRequestExecutor(ComponentProperties props) {
-		final RequestExecutor baseRequestExecutor = getBaseRequestExecutor(props);
-		Retry retry = getRetry(props);
-		RateLimiter rateLimiter = getRateLimiter(props);
-		return RequestExecutorDecorator.decorate(baseRequestExecutor).with(retry).with(rateLimiter).get();
-	}
+    public RequestExecutor getRequestExecutor(ComponentProperties props) {
+        final RequestExecutor baseRequestExecutor = getBaseRequestExecutor(props);
+        Retry retry = getRetry(props);
+        RateLimiter rateLimiter = getRateLimiter(props);
+        return RequestExecutorDecorator.decorate(baseRequestExecutor).with(retry).with(rateLimiter).get();
+    }
 
-	private RateLimiter getRateLimiter(ComponentProperties props) {
-		boolean rateLimitEnabled = props.getOptionalBoolean(RATE_LIMIT_ENABLED).orElse(Boolean.FALSE);
-		if (rateLimitEnabled) {
-			int maxRequestsPerMinute = props.getOptionalInteger(MAX_REQUESTS_PER_MINUTE).orElse(500);
-			return RateLimiterConfig.limitPerMinute(maxRequestsPerMinute).getRateLimiter();
-		} else {
-			return null;
-		}
-	}
+    private RateLimiter getRateLimiter(ComponentProperties props) {
+        boolean rateLimitEnabled = props.getOptionalBoolean(RATE_LIMIT_ENABLED).orElse(Boolean.FALSE);
+        if (!rateLimitEnabled) {
+            return null;
+        }
 
-	private Retry getRetry(ComponentProperties props) {
-		boolean retriesEnabled = props.getOptionalBoolean(RETRIES_ENABLED).orElse(Boolean.TRUE);
-		if (retriesEnabled) {
-			int maxRetries = props.getOptionalInteger(MAX_RETRIES).orElse(5);
-			List<Integer> statusesToRetry = props.getOptionalProperty(STATUSES_TO_RETRY)
-					.map(csv -> Stream.of(csv.split(",")).map(String::trim).map(Integer::parseInt).toList())
-					.orElse(new ArrayList<>());
-			return RetryConfig.of(maxRetries, statusesToRetry).getRetry();
-		} else {
-			return null;
-		}
-	}
+        return props.getOptionalInteger(MAX_REQUESTS_PER_MINUTE)
+                .map(maxRequestsPerMinute -> RateLimiterConfig.limitPerMinute(maxRequestsPerMinute).getRateLimiter())
+                .orElseGet(() -> {
+                    String period = props.getOptionalProperty(RATE_LIMIT_PERIOD).orElse(DEFAULT_RATE_LIMIT_PERIOD);
+                    int limit = props.getOptionalInteger(RATE_LIMIT_LIMIT).orElse(500);
+                    return RateLimiterConfig.limitForPeriod(limit, period).getRateLimiter();
+                });
+    }
 
-	private RequestExecutor getBaseRequestExecutor(ComponentProperties componentProperties) {
-		Optional<AuthStrategy> authentication = AuthStrategy
-				.from(componentProperties.getOptionalProperty(AUTH_TYPE).orElse(NO_AUTH.name()));
-		final List<Header> headers = getHttpHeaders(componentProperties);
+    private Retry getRetry(ComponentProperties props) {
+        boolean retriesEnabled = props.getOptionalBoolean(RETRIES_ENABLED).orElse(Boolean.TRUE);
+        if (retriesEnabled) {
+            int maxRetries = props.getOptionalInteger(MAX_RETRIES).orElse(5);
+            List<Integer> statusesToRetry = props.getOptionalProperty(STATUSES_TO_RETRY)
+                    .map(csv -> Stream.of(csv.split(",")).map(String::trim).map(Integer::parseInt).toList())
+                    .orElse(new ArrayList<>());
+            return RetryConfig.of(maxRetries, statusesToRetry).getRetry();
+        } else {
+            return null;
+        }
+    }
 
-		if (authentication.isPresent()) {
-			return switch (authentication.get()) {
-				case NO_AUTH -> requestExecutorFactory.createNoAuthExecutor(headers);
-				case API_KEY -> {
-					String apiKeyHeader = componentProperties
-							.getOptionalProperty(API_KEY_HEADER)
-							.orElse(DEFAULT_API_KEY_HEADER);
-					String apiKey = componentProperties.getProperty(API_KEY);
+    private RequestExecutor getBaseRequestExecutor(ComponentProperties componentProperties) {
+        Optional<AuthStrategy> authentication = AuthStrategy
+                .from(componentProperties.getOptionalProperty(AUTH_TYPE).orElse(NO_AUTH.name()));
+        final List<Header> headers = getHttpHeaders(componentProperties);
 
-					List<Header> headersWithApiKey = new ArrayList<>(headers);
-					headersWithApiKey.add(new BasicHeader(apiKeyHeader, apiKey));
-					yield requestExecutorFactory.createNoAuthExecutor(headersWithApiKey);
-				}
-				case OAUTH2_CLIENT_CREDENTIALS ->
-					requestExecutorFactory.createClientCredentialsExecutor(
-							headers,
-							componentProperties.getProperty(CLIENT_ID),
-							componentProperties.getProperty(CLIENT_SECRET),
-							componentProperties.getProperty(TOKEN_ENDPOINT));
-			};
-		}
-		throw new UnsupportedOperationException("Requested authentication not available: "
-				+ componentProperties.getOptionalProperty(AUTH_TYPE).orElse("No auth type provided"));
-	}
+        if (authentication.isPresent()) {
+            return switch (authentication.get()) {
+                case NO_AUTH -> requestExecutorFactory.createNoAuthExecutor(headers);
+                case API_KEY -> {
+                    String apiKeyHeader = componentProperties
+                            .getOptionalProperty(API_KEY_HEADER)
+                            .orElse(DEFAULT_API_KEY_HEADER);
+                    String apiKey = componentProperties.getProperty(API_KEY);
 
-	private List<Header> getHttpHeaders(ComponentProperties componentProperties) {
-		final ComponentProperties headers = componentProperties.extractNestedProperties(HTTP_HEADERS);
-		final List<Header> result = new ArrayList<>();
-		for (int i = 0; isNotEmpty(headers.extractNestedProperties(String.valueOf(i)).getConfig()); i++) {
-			ComponentProperties headerProperties = headers.extractNestedProperties(String.valueOf(i));
-			BasicHeader basicHeader = new BasicHeader(
-					headerProperties.getProperty(HTTP_HEADERS_KEY),
-					headerProperties.getProperty(HTTP_HEADERS_VALUE)
-			);
-			result.add(basicHeader);
-		}
-		return result;
-	}
+                    List<Header> headersWithApiKey = new ArrayList<>(headers);
+                    headersWithApiKey.add(new BasicHeader(apiKeyHeader, apiKey));
+                    yield requestExecutorFactory.createNoAuthExecutor(headersWithApiKey);
+                }
+                case OAUTH2_CLIENT_CREDENTIALS -> requestExecutorFactory.createClientCredentialsExecutor(
+                        headers,
+                        componentProperties.getProperty(CLIENT_ID),
+                        componentProperties.getProperty(CLIENT_SECRET),
+                        componentProperties.getProperty(TOKEN_ENDPOINT));
+            };
+        }
+        throw new UnsupportedOperationException("Requested authentication not available: "
+                + componentProperties.getOptionalProperty(AUTH_TYPE).orElse("No auth type provided"));
+    }
+
+    private List<Header> getHttpHeaders(ComponentProperties componentProperties) {
+        final ComponentProperties headers = componentProperties.extractNestedProperties(HTTP_HEADERS);
+        final List<Header> result = new ArrayList<>();
+        for (int i = 0; isNotEmpty(headers.extractNestedProperties(String.valueOf(i)).getConfig()); i++) {
+            ComponentProperties headerProperties = headers.extractNestedProperties(String.valueOf(i));
+            BasicHeader basicHeader = new BasicHeader(
+                    headerProperties.getProperty(HTTP_HEADERS_KEY),
+                    headerProperties.getProperty(HTTP_HEADERS_VALUE)
+            );
+            result.add(basicHeader);
+        }
+        return result;
+    }
 
 }
