e1f68fd3a396291257d6f5913385980181a23035
tomvbe <tom.vanbesien@gmail.com>
2024-01-25 14:43:02 +0100

    Add document cache to rdfadapter

--- ldi-core/rdf-adapter/src/main/java/be/vlaanderen/informatievlaanderen/ldes/ldi/RdfAdapter.java
+++ ldi-core/rdf-adapter/src/main/java/be/vlaanderen/informatievlaanderen/ldes/ldi/RdfAdapter.java
@@ -1,18 +1,37 @@
 package be.vlaanderen.informatievlaanderen.ldes.ldi;
 
 import be.vlaanderen.informatievlaanderen.ldes.ldi.types.LdiAdapter;
+import com.apicatalog.jsonld.JsonLdOptions;
+import com.apicatalog.jsonld.context.cache.LruCache;
 import org.apache.jena.rdf.model.Model;
+import org.apache.jena.riot.RDFParser;
 import org.apache.jena.riot.RDFParserBuilder;
+import org.apache.jena.riot.RIOT;
+import org.apache.jena.sparql.util.Context;
+import org.apache.jena.sparql.util.ContextAccumulator;
 
 import java.util.stream.Stream;
 
 import static org.apache.jena.riot.RDFLanguages.nameToLang;
+import static org.apache.jena.riot.lang.LangJSONLD11.JSONLD_OPTIONS;
 
 public class RdfAdapter implements LdiAdapter {
 
-	@Override
+	private final Context context;
+
+    public RdfAdapter(Context context) {
+        this.context = context;
+    }
+
+    @Override
 	public Stream<Model> apply(Content input) {
 		return Stream.of(
-				RDFParserBuilder.create().fromString(input.content()).lang(nameToLang(input.mimeType())).toModel());
+				RDFParser
+						.fromString(input.content())
+						.context(context)
+						.lang(nameToLang(input.mimeType()))
+						.toModel()
+		);
 	}
+
 }
--- ldi-orchestrator/ldio-connectors/ldio-rdf-adapter/src/main/java/be/vlaanderen/informatievlaanderen/ldes/ldio/config/LdioRdfAdapterAutoConfig.java
+++ ldi-orchestrator/ldio-connectors/ldio-rdf-adapter/src/main/java/be/vlaanderen/informatievlaanderen/ldes/ldio/config/LdioRdfAdapterAutoConfig.java
@@ -4,11 +4,20 @@ import be.vlaanderen.informatievlaanderen.ldes.ldi.RdfAdapter;
 import be.vlaanderen.informatievlaanderen.ldes.ldi.types.LdiAdapter;
 import be.vlaanderen.informatievlaanderen.ldes.ldio.configurator.LdioAdapterConfigurator;
 import be.vlaanderen.informatievlaanderen.ldes.ldio.valueobjects.ComponentProperties;
+import com.apicatalog.jsonld.JsonLdOptions;
+import com.apicatalog.jsonld.context.cache.LruCache;
+import org.apache.jena.riot.RIOT;
+import org.apache.jena.sparql.util.Context;
+import org.apache.jena.sparql.util.ContextAccumulator;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import static org.apache.jena.riot.lang.LangJSONLD11.JSONLD_OPTIONS;
+
 @Configuration
 public class LdioRdfAdapterAutoConfig {
+
+	public static final String MAX_JSONLD_CACHE_CAPACITY = "max-jsonld-cache-capacity";
 
 	@SuppressWarnings("java:S6830")
 	@Bean("be.vlaanderen.informatievlaanderen.ldes.ldi.RdfAdapter")
@@ -17,9 +26,18 @@ public class LdioRdfAdapterAutoConfig {
 	}
 
 	public static class LdioRdfConfigurator implements LdioAdapterConfigurator {
+
 		@Override
 		public LdiAdapter configure(ComponentProperties config) {
-			return new RdfAdapter();
+			final int maxCacheCapacity = config.getOptionalInteger(MAX_JSONLD_CACHE_CAPACITY).orElse(100);
+			return new RdfAdapter(prepareContext(maxCacheCapacity));
 		}
+
+		private Context prepareContext(final int maxCacheCapacity) {
+			final var options = new JsonLdOptions();
+			options.setDocumentCache(new LruCache<>(maxCacheCapacity));
+            return ContextAccumulator.newBuilder(RIOT::getContext).context().set(JSONLD_OPTIONS, options);
+		}
+
 	}
 }
