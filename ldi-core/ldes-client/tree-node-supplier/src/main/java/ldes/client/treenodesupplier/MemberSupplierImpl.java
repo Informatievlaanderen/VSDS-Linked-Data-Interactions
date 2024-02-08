db44731c71a6697f4dacbe0d7c8b032e9b71786b
Yalz <Yalz@users.noreply.github.com>
2024-02-08 16:11:12 +0100

    cleanup

--- ldi-core/ldes-client/tree-node-supplier/src/main/java/ldes/client/treenodesupplier/MemberSupplierImpl.java
+++ ldi-core/ldes-client/tree-node-supplier/src/main/java/ldes/client/treenodesupplier/MemberSupplierImpl.java
@@ -1,20 +1,11 @@
 package ldes.client.treenodesupplier;
 
 import ldes.client.treenodesupplier.domain.valueobject.SuppliedMember;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.TimeUnit;
 
 public class MemberSupplierImpl implements MemberSupplier {
 
-    private static final long DEFAULT_WAITING_TIME_IN_SECONDS = 100;
     private final TreeNodeProcessor treeNodeProcessor;
     private final boolean keepState;
-    private final Logger logger = LoggerFactory.getLogger(MemberSupplierImpl.class);
-    private final ExecutorService executorService = Executors.newSingleThreadExecutor();
 
     public MemberSupplierImpl(TreeNodeProcessor treeNodeProcessor, boolean keepState) {
         this.treeNodeProcessor = treeNodeProcessor;
@@ -29,18 +20,8 @@ public class MemberSupplierImpl implements MemberSupplier {
 
     @Override
     public void destroyState() {
-        try {
-            executorService.shutdown();
-            if (!executorService.awaitTermination(DEFAULT_WAITING_TIME_IN_SECONDS, TimeUnit.SECONDS)) {
-                logger.warn("ExecutorService wasn't able to finish last task.");
-                System.exit(0);
-            }
-            if (!keepState) {
-                treeNodeProcessor.destroyState();
-            }
-        } catch (InterruptedException e) {
-            Thread.currentThread().interrupt();
-            throw new ClientInterruptedException(e);
+        if (!keepState) {
+            treeNodeProcessor.destroyState();
         }
     }
 
