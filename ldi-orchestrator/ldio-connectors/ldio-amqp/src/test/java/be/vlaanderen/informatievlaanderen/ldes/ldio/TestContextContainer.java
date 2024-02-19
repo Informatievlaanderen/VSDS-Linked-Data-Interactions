16fa36f22e7b8c11f5dc7dd90d4fd916ba9a9a58
tomvbe <tom.vanbesien@gmail.com>
2024-02-19 12:23:54 +0100

    Remove useless annotation

--- ldi-orchestrator/ldio-connectors/ldio-amqp/src/test/java/be/vlaanderen/informatievlaanderen/ldes/ldio/TestContextContainer.java
+++ ldi-orchestrator/ldio-connectors/ldio-amqp/src/test/java/be/vlaanderen/informatievlaanderen/ldes/ldio/TestContextContainer.java
@@ -1,13 +1,11 @@
 package be.vlaanderen.informatievlaanderen.ldes.ldio;
 
-import io.cucumber.java.After;
 import io.cucumber.java.Before;
 
 public class TestContextContainer {
 
     private static TestContext testContext = null;
 
-    @After
     @Before
     public static void setupTestContext() {
         testContext = new TestContext();
