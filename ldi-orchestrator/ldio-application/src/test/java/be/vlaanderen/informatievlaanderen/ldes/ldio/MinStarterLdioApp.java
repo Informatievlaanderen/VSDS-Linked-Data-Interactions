01b0b864b4de9053c9baecd40eef5953d3f4eed8
Yalz <Yalz@users.noreply.github.com>
2024-02-21 15:44:52 +0100

    feat: dynamic pipelines: integration tests

--- ldi-orchestrator/ldio-application/src/test/java/be/vlaanderen/informatievlaanderen/ldes/ldio/MinStarterLdioApp.java
+++ ldi-orchestrator/ldio-application/src/test/java/be/vlaanderen/informatievlaanderen/ldes/ldio/MinStarterLdioApp.java
@@ -1,13 +1,9 @@
 package be.vlaanderen.informatievlaanderen.ldes.ldio;
 
-import org.hibernate.service.ServiceRegistry;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.context.annotation.Bean;
 import org.springframework.web.context.ConfigurableWebApplicationContext;
-
-import static org.mockito.Mockito.mock;
 
 public class MinStarterLdioApp {
 	static ConfigurableWebApplicationContext setupApp(String file) {
@@ -19,10 +15,5 @@ public class MinStarterLdioApp {
 				.build();
 
 		return (ConfigurableWebApplicationContext) app.run();
-	}
-
-	@Bean
-	ServiceRegistry serviceRegistry() {
-		return mock(ServiceRegistry.class);
 	}
 }
