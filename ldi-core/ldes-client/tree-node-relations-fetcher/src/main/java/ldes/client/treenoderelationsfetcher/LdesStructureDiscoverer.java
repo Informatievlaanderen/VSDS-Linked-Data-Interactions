28adfbfd82ff13f96e48b1946394c6c9d54f3869
jonasbul <jonas.bulcke@cegeka.com>
2024-02-22 13:19:53 +0100

    feat: code review implemented

--- ldi-core/ldes-client/tree-node-relations-fetcher/src/main/java/ldes/client/treenoderelationsfetcher/LdesStructureDiscoverer.java
+++ ldi-core/ldes-client/tree-node-relations-fetcher/src/main/java/ldes/client/treenoderelationsfetcher/LdesStructureDiscoverer.java
@@ -13,45 +13,46 @@ import java.util.List;
 public class LdesStructureDiscoverer {
 	private static final Logger log = LoggerFactory.getLogger(LdesStructureDiscoverer.class);
 	private final TreeRelationsFetcher treeNodeFetcher;
+	private final String startingUrl;
 	private final Lang sourceFormat;
 	private final RequestExecutor requestExecutor;
-	private final LdesStructure ldesStructure;
 
 	public LdesStructureDiscoverer(String startingUrl, Lang sourceFormat, RequestExecutor requestExecutor) {
+		this.startingUrl = startingUrl;
 		this.sourceFormat = sourceFormat;
 		this.requestExecutor = requestExecutor;
 		this.treeNodeFetcher = new TreeRelationsFetcher(requestExecutor);
-		this.ldesStructure = new LdesStructure(startingUrl);
 	}
 
 	public LdesStructure discoverLdesStructure() {
+		final LdesStructure ldesStructure = new LdesStructure(startingUrl);
 		getStartingTreeRelations()
 				.parallelStream()
 				.forEach(relation -> {
 					ldesStructure.addRelation(relation);
-					fetchRelations(relation);
+					fetchRelations(relation, true);
 				});
 		return ldesStructure;
 	}
 
 	private List<TreeRelation> getStartingTreeRelations() {
 		return new StartingTreeNodeUrlSupplier(requestExecutor)
-				.getStartingTreeNodeRelations(ldesStructure.getUri(), sourceFormat)
+				.getStartingTreeNodeRelations(startingUrl, sourceFormat)
 				.stream()
 				.map(startingNode -> new TreeRelation(startingNode.getUrl(), true))
 				.toList();
 	}
 
 
-	private void fetchRelations(TreeRelation relation) {
+	private void fetchRelations(TreeRelation relation, boolean isChildOfRoot) {
 		final TreeNodeRequest treeNodeRequest = new TreeNodeRequest(relation.getUri(), sourceFormat);
 		try {
 			treeNodeFetcher.fetchTreeRelations(treeNodeRequest)
 					.parallelStream()
 					.forEach(fetchedRelation -> {
-						if(fetchedRelation.isRequired() || ldesStructure.containsChild(relation)) {
+						if (fetchedRelation.isRequired() || isChildOfRoot) {
 							relation.addRelation(fetchedRelation);
-							fetchRelations(fetchedRelation);
+							fetchRelations(fetchedRelation, false);
 						}
 					});
 			log.debug("{} relation(s) found for nodeUrl {}", relation.countChildRelations(), relation.getUri());
