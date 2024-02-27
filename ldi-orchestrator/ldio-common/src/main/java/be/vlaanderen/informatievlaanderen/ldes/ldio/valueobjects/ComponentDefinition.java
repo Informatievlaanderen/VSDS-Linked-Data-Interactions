ffab80565d862b3fde03e66f3042e55461b54460
Tomvbe <34196062+Tomvbe@users.noreply.github.com>
2024-02-27 15:01:14 +0100

    fix: invalid pipeline config (#515)

--- ldi-orchestrator/ldio-common/src/main/java/be/vlaanderen/informatievlaanderen/ldes/ldio/valueobjects/ComponentDefinition.java
+++ ldi-orchestrator/ldio-common/src/main/java/be/vlaanderen/informatievlaanderen/ldes/ldio/valueobjects/ComponentDefinition.java
@@ -3,29 +3,19 @@ package be.vlaanderen.informatievlaanderen.ldes.ldio.valueobjects;
 import java.util.Map;
 
 public class ComponentDefinition {
-	private String pipelineName;
-	private String name;
-	private ComponentProperties config = new ComponentProperties(name, pipelineName);
+
+	private final String name;
+	private final ComponentProperties config;
 
 	public ComponentDefinition(String pipelineName, String name, Map<String, String> config) {
-		this.pipelineName = pipelineName;
 		this.name = name;
-		this.config = new ComponentProperties(pipelineName, name, config);
-	}
-
-	public ComponentDefinition() {
+		this.config = config != null
+				? new ComponentProperties(pipelineName, name, config)
+				: new ComponentProperties(pipelineName, name);
 	}
 
 	public String getName() {
 		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
-
-	public void setPipelineName(String pipelineName) {
-		this.pipelineName = pipelineName;
 	}
 
 	public ComponentProperties getConfig() {
@@ -36,11 +26,4 @@ public class ComponentDefinition {
 		return config.getConfig();
 	}
 
-	public void setConfig(Map<String, String> config) {
-		this.config = new ComponentProperties(pipelineName, name, config);
-	}
-
-	public void setConfig(ComponentProperties config) {
-		this.config = config;
-	}
 }
