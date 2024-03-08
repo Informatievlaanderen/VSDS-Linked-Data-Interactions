90d37a0d9a00ea79a319d8b1da167eb55c236116
jonasbul <jonas.bulcke@cegeka.com>
2024-03-08 11:13:52 +0100

    feat: additional tests

---
+++ ldi-orchestrator/ldio-application/src/test/java/be/vlaanderen/informatievlaanderen/ldes/ldio/controller/PipelineControllerTest.java
@@ -0,0 +1,83 @@
+package be.vlaanderen.informatievlaanderen.ldes.ldio.controller;
+
+import be.vlaanderen.informatievlaanderen.ldes.ldio.config.OrchestratorConfig;
+import be.vlaanderen.informatievlaanderen.ldes.ldio.repositories.PipelineFileRepository;
+import be.vlaanderen.informatievlaanderen.ldes.ldio.services.PipelineCreatorService;
+import be.vlaanderen.informatievlaanderen.ldes.ldio.services.PipelineService;
+import be.vlaanderen.informatievlaanderen.ldes.ldio.services.PipelineStatusService;
+import org.apache.commons.io.FileUtils;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.util.ResourceUtils;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest
+@ComponentScan({
+		"be.vlaanderen.informatievlaanderen.ldes.ldio",
+		"be.vlaanderen.informatievlaanderen.ldes.ldio.config"
+})
+@ActiveProfiles("rest")
+@ContextConfiguration(classes = {PipelineController.class, PipelineService.class, PipelineStatusService.class,
+		PipelineCreatorService.class, OrchestratorConfig.class, PipelineFileRepository.class})
+class PipelineControllerTest {
+	@Autowired
+	private MockMvc mockMvc;
+
+	@Test
+	void given_PredefinedPipeline_when_GetOverview_And_JSON_then_ReturnJson() throws Exception {
+		mockMvc.perform(get("/admin/api/v1/pipeline").accept("application/json"))
+				.andExpect(status().isOk())
+				.andExpect(content().json(readAllPipelinesInJson("pipelines/pre-defined-pipeline.json")));
+	}
+
+	@Test
+	void given_ValidPipelineConfig_when_PostPipeline_then_ReturnPipelineWithHttpStatus200() throws Exception {
+		mockMvc.perform(
+						post("/admin/api/v1/pipeline")
+								.contentType("application/yaml")
+								.content(readPipeline("pipelines/client-pipeline.yaml"))
+								.accept("application/json"))
+				.andExpect(status().isOk())
+				.andExpect(content().json(readPipeline("pipelines/client-pipeline.json")));
+	}
+
+	@Test
+	void given_PipelineConfigWithMissingLdiAdapter_when_PostPipeline_ReturnStatus400() throws Exception {
+		mockMvc.perform(
+						post("/admin/api/v1/pipeline")
+								.contentType("application/yaml")
+								.content(readPipeline("pipelines/invalid-pipeline.yaml"))
+								.accept("application/yaml"))
+				.andExpect(status().isBadRequest())
+				.andExpect(content().string("Pipeline \"invalid-pipeline\": Input: \"Ldio:HttpIn\": Missing LDI Adapter"));
+	}
+
+	private String readAllPipelinesInJson(String... filenames) {
+		return "[" + Stream.of(filenames)
+				.map(this::readPipeline)
+				.collect(Collectors.joining(", ")) + "]";
+	}
+
+	private String readPipeline(String filename) {
+		try {
+			final File jsonFile = ResourceUtils.getFile("classpath:" + filename);
+			return FileUtils.readFileToString(jsonFile, "utf-8");
+		} catch (IOException e) {
+			throw new RuntimeException(e);
+		}
+	}
+}
