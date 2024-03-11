d614a28840748dc4c73181124b4af1dc9cd5cf64
Jonas Bulcke <127748878+jobulcke@users.noreply.github.com>
2024-03-11 17:01:24 +0100

    fix: gh 501 (#536)

--- ldi-core/repository-materialiser/src/main/java/be/vlaanderen/informatievlaanderen/ldes/ldi/exceptions/MaterialisationFailedException.java
+++ ldi-core/repository-materialiser/src/main/java/be/vlaanderen/informatievlaanderen/ldes/ldi/exceptions/MaterialisationFailedException.java
@@ -1,24 +1,12 @@
 package be.vlaanderen.informatievlaanderen.ldes.ldi.exceptions;
 
-import org.apache.jena.rdf.model.Model;
-import org.apache.jena.riot.Lang;
-import org.apache.jena.riot.RDFWriter;
-
-import java.util.List;
-import java.util.stream.Collectors;
-
 public class MaterialisationFailedException extends RuntimeException {
-	private final transient List<Model> uncommittedModels;
-	public MaterialisationFailedException(Exception e, List<Model> uncommittedModels) {
+	public MaterialisationFailedException(Exception e) {
 		super(e);
-		this.uncommittedModels = uncommittedModels;
 	}
 
 	@Override
 	public String getMessage() {
-		final String uncommittedMembers = uncommittedModels.stream()
-				.map(model -> RDFWriter.source(model).lang(Lang.TURTLE).asString())
-				.collect(Collectors.joining("\n"));
-		return "The following members could not be materialised to the triples store%n%n%s".formatted(uncommittedMembers);
+		return "Materialisation to triples store failed: %s".formatted(getCause().getMessage());
 	}
 }
