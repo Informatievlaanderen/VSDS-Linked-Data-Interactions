61b3bc8678fba76f13daa52cbfc462cb31dd3d0e
pj-cegeka <pieterjan.livens@cegeka.com>
2024-04-08 09:32:28 +0200

    fix: error message

--- ldi-nifi/ldi-nifi-processors/rdf4j-repository-materialisation-processor/src/main/java/be/vlaanderen/informatievlaanderen/ldes/ldi/processors/RDF4JRepositoryMaterialisationProcessor.java
+++ ldi-nifi/ldi-nifi-processors/rdf4j-repository-materialisation-processor/src/main/java/be/vlaanderen/informatievlaanderen/ldes/ldi/processors/RDF4JRepositoryMaterialisationProcessor.java
@@ -89,7 +89,8 @@ public class RDF4JRepositoryMaterialisationProcessor extends AbstractProcessor {
 
 			session.transfer(flowFiles, SUCCESS);
 		} catch (Exception e) {
-			getLogger().error("Error sending model to repository", e.getMessage());
+			getLogger().error(e.getMessage());
+			getLogger().error("Error sending model to repository: {}", e.getMessage());
 			session.transfer(flowFiles, FAILURE);
 		}
 	}
