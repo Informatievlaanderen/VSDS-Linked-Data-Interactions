a0b6d9fdab9428d7a01e5d05c6076bb6ab98ab85
Ferre Vaes <devferre@gmail.com>
2024-04-26 17:10:32 +0200

    feat: expand integration tests for LDES client with latest state filter and exactly once filter

--- ldi-orchestrator/ldio-connectors/ldio-ldes-client/src/test/java/be/vlaanderen/informatievlaanderen/ldes/ldio/config/LdioLdesClientITSteps.java
+++ ldi-orchestrator/ldio-connectors/ldio-ldes-client/src/test/java/be/vlaanderen/informatievlaanderen/ldes/ldio/config/LdioLdesClientITSteps.java
@@ -3,6 +3,7 @@ package be.vlaanderen.informatievlaanderen.ldes.ldio.config;
 import be.vlaanderen.informatievlaanderen.ldes.ldi.services.ComponentExecutor;
 import be.vlaanderen.informatievlaanderen.ldes.ldio.LdioLdesClientProperties;
 import be.vlaanderen.informatievlaanderen.ldes.ldio.valueobjects.ComponentProperties;
+import com.github.tomakehurst.wiremock.WireMockServer;
 import io.cucumber.java.BeforeAll;
 import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
@@ -18,18 +19,19 @@ import java.util.List;
 import java.util.Map;
 import java.util.concurrent.atomic.AtomicInteger;
 
-import com.github.tomakehurst.wiremock.WireMockServer;
-
 import static be.vlaanderen.informatievlaanderen.ldes.ldio.LdioLdesClient.NAME;
 import static be.vlaanderen.informatievlaanderen.ldes.ldio.LdioLdesClientProperties.URLS;
 import static com.github.tomakehurst.wiremock.core.WireMockConfiguration.options;
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.awaitility.Awaitility.await;
 
 public class LdioLdesClientITSteps extends LdesClientInIT {
-	private ApplicationEventPublisher applicationEventPublisher = applicationEventPublisher();
-	private List<Model> members;
-	static WireMockServer wireMockServer = new WireMockServer(options().port(10101));
+	private final static WireMockServer wireMockServer = new WireMockServer(options().port(10101));
+
+	private final ApplicationEventPublisher applicationEventPublisher = applicationEventPublisher();
 	private final Map<String, String> componentPropsMap = new HashMap<>();
+	private final List<Model> members = new ArrayList<>();
+
 
 	@BeforeAll
 	public static void before_all() {
@@ -53,8 +55,8 @@ public class LdioLdesClientITSteps extends LdesClientInIT {
 
 	@When("^I start an ldes-ldio-in component")
 	public void iStartAnLdesLdioInComponentWithUrl() {
-		members = new ArrayList<>();
-		ComponentExecutor componentExecutor = linkedDataModel -> members.add(linkedDataModel);
+		members.clear();
+		ComponentExecutor componentExecutor = members::add;
 
 		var props = new ComponentProperties("pipelineName", NAME, componentPropsMap);
 		var ldioInputConfigurator = new LdioLdesClientAutoConfig().ldioConfigurator(null);
@@ -63,10 +65,16 @@ public class LdioLdesClientITSteps extends LdesClientInIT {
 
 	@Then("All {int} members from the stream are passed to the pipeline")
 	public void allMembersFromTheStreamArePassedToThePipeline(int memberCount) {
-		await().atMost(Duration.ofMinutes(2)).until(() -> {
-			System.out.println(members.size());
-			return members.size() == memberCount;
+		await().atMost(Duration.ofMinutes(2)).untilAsserted(() -> {
+			synchronized (members) {
+				System.out.println(members.size());
+				assertThat(members).hasSize(memberCount);
+			}
 		});
 	}
 
+	@And("I want to add the following properties")
+	public void iWantToConfigureTheFollowingProperties(Map<String, String> properties) {
+		this.componentPropsMap.putAll(properties);
+	}
 }
--- ldi-orchestrator/ldio-connectors/ldio-ldes-client/src/test/resources/features/ldes-client-in.feature
+++ ldi-orchestrator/ldio-connectors/ldio-ldes-client/src/test/resources/features/ldes-client-in.feature
@@ -16,8 +16,34 @@ Feature: LdesClientIntegrationTest
     When I start an ldes-ldio-in component
     Then All 8 members from the stream are passed to the pipeline
 
-  # TODO: add Latest state test
+  Scenario Outline: Reading an LDES Stream with latest state filter enabled
+    Given I want to follow the following LDES
+      | /exampleData?generatedAtTime=2022-05-03T00:00:00.000Z |
+    And I want to add the following properties
+      | materialisation.enabled                    | true                                      |
+      | materialisation.enable-latest-state-filter | <isLatestStateFilterEnabled>              |
+      | materialisation.version-of-property        | http://purl.org/dc/terms/isVersionOf      |
+      | timestamp-path                             | http://www.w3.org/ns/prov#generatedAtTime |
+    And I configure this to be of RDF format "application/ld+json"
+    When I start an ldes-ldio-in component
+    Then All <expectedMemberSize> members from the stream are passed to the pipeline
 
-  # TODO: add Without Latest state filter
+    Examples:
+      | isLatestStateFilterEnabled | expectedMemberSize |
+      | true                       | 3                  |
+      | false                      | 6                  |
 
-  # TODO: add exactly once
+
+  Scenario Outline: Reading an LDES Stream with exactly once filter enabled
+    Given I want to follow the following LDES
+      | /items/grouped?group=1 |
+      | /items/grouped?group=3 |
+    And I want to add the following properties
+      | enable-exactly-once | <isExactlyOnceFilterEnabled> |
+    When I start an ldes-ldio-in component
+    Then All <expectedMemberSize> members from the stream are passed to the pipeline
+
+    Examples:
+      | isExactlyOnceFilterEnabled | expectedMemberSize |
+      | true                       | 4                  |
+      | false                      | 8                  |
