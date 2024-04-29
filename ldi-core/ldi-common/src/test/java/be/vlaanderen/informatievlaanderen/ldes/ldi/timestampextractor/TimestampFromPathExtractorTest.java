8ebb554b9fd20fcfcc01e887a33a307201e1dbc9
Jonas Bulcke <jonas.bulcke@cegeka.com>
2024-04-29 13:33:35 +0200

    fix: broken test

--- ldi-core/ldi-common/src/test/java/be/vlaanderen/informatievlaanderen/ldes/ldi/timestampextractor/TimestampFromPathExtractorTest.java
+++ ldi-core/ldi-common/src/test/java/be/vlaanderen/informatievlaanderen/ldes/ldi/timestampextractor/TimestampFromPathExtractorTest.java
@@ -9,12 +9,13 @@ import org.junit.jupiter.api.Test;
 
 import java.time.LocalDateTime;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class TimestampFromPathExtractorTest {
 
 	public static final Property TIME_PROPERTY = ResourceFactory.createProperty("http://www.w3.org/ns/prov#generatedAtTime");
+	public static final Property INVALID_TIME_PROPERTY = ResourceFactory.createProperty("not-existing-property");
 	public TimestampFromPathExtractor timestampFromPathExtractor;
 	private Model inputModel;
 	private Property memberSubject;
@@ -23,31 +24,41 @@ class TimestampFromPathExtractorTest {
 	void setUp() {
 		inputModel = RDFParser.source("model-with-timestamp20220520.nq").toModel();
 		memberSubject = ResourceFactory.createProperty("https://private-api.gipod.beta-vlaanderen.be/api/v1/mobility-hindrances/10810464/1");
-		timestampFromPathExtractor = new TimestampFromPathExtractor(TIME_PROPERTY);
 	}
 
 	@Test
 	void ExtractTimestamp_ShouldThrowException_WhenTimestampNotFound() {
-		assertThrows(IllegalArgumentException.class, () -> timestampFromPathExtractor.extractTimestamp(inputModel));
-	}
+		timestampFromPathExtractor = new TimestampFromPathExtractor(INVALID_TIME_PROPERTY);
+
+		assertThatThrownBy(() -> timestampFromPathExtractor.extractTimestamp(inputModel))
+				.isInstanceOf(IllegalArgumentException.class)
+				.hasMessage("No timestamp as literal found on member");	}
 
 	@Test
 	void ExtractTimestamp_ShouldTimestamp_WhenFound() {
+		timestampFromPathExtractor = new TimestampFromPathExtractor(TIME_PROPERTY);
+
 		LocalDateTime result = timestampFromPathExtractor.extractTimestamp(inputModel);
 
-		assertEquals(LocalDateTime.of(2022, 5, 20, 9, 58, 15), result);
+		assertThat(result).isEqualTo("2022-05-20T09:58:15");
 	}
 
 	@Test
 	void ExtractTimestampWithSubject_ShouldThrowException_WhenTimestampNotFound() {
-		assertThrows(IllegalArgumentException.class, () -> timestampFromPathExtractor.extractTimestampWithSubject(memberSubject, inputModel));
+		timestampFromPathExtractor = new TimestampFromPathExtractor(INVALID_TIME_PROPERTY);
+
+		assertThatThrownBy(() -> timestampFromPathExtractor.extractTimestampWithSubject(memberSubject, inputModel))
+				.isInstanceOf(IllegalArgumentException.class)
+				.hasMessage("No timestamp as literal found on member");
 	}
 
 	@Test
 	void ExtractTimestampWithSubject_ShouldTimestamp_WhenFound() {
+		timestampFromPathExtractor = new TimestampFromPathExtractor(TIME_PROPERTY);
+
 		LocalDateTime result = timestampFromPathExtractor.extractTimestampWithSubject(memberSubject, inputModel);
 
-		assertEquals(LocalDateTime.of(2022, 5, 20, 9, 58, 15), result);
+		assertThat(result).isEqualTo("2022-05-20T09:58:15");
 	}
 
 }
