01f41f04cb1dcf5b45eedc6aaa87ef359c60acc2
Jonas Bulcke <127748878+jobulcke@users.noreply.github.com>
2024-05-07 10:33:27 +0200

    feat: adding logging statement when a member is ignored (#627)

--- ldi-core/ldes-client/tree-node-supplier/src/main/java/ldes/client/treenodesupplier/membersuppliers/FilteredMemberSupplier.java
+++ ldi-core/ldes-client/tree-node-supplier/src/main/java/ldes/client/treenodesupplier/membersuppliers/FilteredMemberSupplier.java
@@ -2,8 +2,11 @@ package ldes.client.treenodesupplier.membersuppliers;
 
 import ldes.client.treenodesupplier.domain.valueobject.SuppliedMember;
 import ldes.client.treenodesupplier.filters.MemberFilter;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class FilteredMemberSupplier extends MemberSupplierDecorator {
+	private static final Logger log = LoggerFactory.getLogger(FilteredMemberSupplier.class);
 	private final MemberFilter filter;
 
 	public FilteredMemberSupplier(MemberSupplier memberSupplier, MemberFilter filter) {
@@ -16,6 +19,7 @@ public class FilteredMemberSupplier extends MemberSupplierDecorator {
 	public SuppliedMember get() {
 		SuppliedMember member = super.get();
 		while (!filter.isAllowed(member)) {
+			log.debug("Member {} has been ignored by the {}", member.getId(), filter.getClass().getSimpleName());
 			member = super.get();
 		}
 		filter.saveAllowedMember(member);
