27a03a69dd9e3e14a4871ced464c28392ffe206a
Jonas Bulcke <jonas.bulcke@cegeka.com>
2024-11-21 13:11:59 +0100

    feat: client asks for gzip content

--- ldi-orchestrator/ldio-connectors/ldio-request-executor/src/main/java/be/vlaanderen/informatievlaanderen/ldes/ldio/requestexecutor/RequestExecutorProperties.java
+++ ldi-orchestrator/ldio-connectors/ldio-request-executor/src/main/java/be/vlaanderen/informatievlaanderen/ldes/ldio/requestexecutor/RequestExecutorProperties.java
@@ -10,6 +10,8 @@ public class RequestExecutorProperties {
     public static final String HTTP_HEADERS_VALUE = "value";
     public static final String HTTP_METHOD = "http.method";
     public static final String HTTP_CONTENT_TYPE = "http.content-type";
+    public static final String HTTP_ACCEPT_ENCODING_HEADER_KEY = "Accept-Encoding";
+    public static final String HTTP_ACCEPT_ENCODING_HEADER_VALUE = "gzip";
 
     public static final String RETRIES_ENABLED = "retries.enabled";
     public static final String MAX_RETRIES = "retries.max";
